87076e36f5420ee9458d97e460a4131bcab4d866
Youngrae Cho <yrc9229@gmail.com>
2024-11-23 07:21:48 +0900

    feat(core-db): jpa repository 추가

---
+++ storage/core-db/src/main/java/com/imstargg/storage/db/core/ClubJpaRepository.java
@@ -0,0 +1,6 @@
+package com.imstargg.storage.db.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubJpaRepository extends JpaRepository<ClubEntity, Long> {
+}
