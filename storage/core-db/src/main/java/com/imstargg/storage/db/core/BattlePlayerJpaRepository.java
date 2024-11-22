4f1bfdbd4d89a08c2f31d038dcc906c2505f918c
Youngrae Cho <yrc9229@gmail.com>
2024-11-23 07:21:48 +0900

    feat(core-db): jpa repository 추가

---
+++ storage/core-db/src/main/java/com/imstargg/storage/db/core/BattlePlayerJpaRepository.java
@@ -0,0 +1,6 @@
+package com.imstargg.storage.db.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BattlePlayerJpaRepository extends JpaRepository<BattlePlayerEntity, Long> {
+}
