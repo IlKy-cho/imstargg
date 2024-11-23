7f0d50278de2ad88ba2939618ac72fbad60857f6
Youngrae Cho <yrc9229@gmail.com>
2024-11-23 09:44:50 +0900

    feat(batch): 기본 세팅

---
+++ batch/src/main/java/com/imstargg/batch/job/RunTimestampIncrementer.java
@@ -0,0 +1,27 @@
+package com.imstargg.batch.job;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersIncrementer;
+
+import java.time.Clock;
+
+public class RunTimestampIncrementer implements JobParametersIncrementer {
+
+    private static final String RUN_TIMESTAMP_KEY = "run.timestamp";
+
+    private final Clock clock;
+
+    public RunTimestampIncrementer(Clock clock) {
+        this.clock = clock;
+    }
+
+    @Override
+    public JobParameters getNext(JobParameters parameters) {
+        JobParameters params = (parameters == null) ? new JobParameters() : parameters;
+
+        return new JobParametersBuilder(params)
+                .addLong(RUN_TIMESTAMP_KEY, clock.millis())
+                .toJobParameters();
+    }
+}
