e6f8519bb339c2e1855e266d9353680f42b335af
Youngrae Cho <yrc9229@gmail.com>
2024-11-23 20:27:22 +0900

    feat(core-enum): 플레이어 업데이트 가능 상태 조건 추가

--- core/core-enum/src/main/java/com/imstargg/core/enums/PlayerStatus.java
+++ core/core-enum/src/main/java/com/imstargg/core/enums/PlayerStatus.java
@@ -1,11 +1,28 @@
 package com.imstargg.core.enums;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public enum PlayerStatus {
 
     UPDATE_NEW,
     REFRESH_NEW,
     NOT_FOUND,
-    UPDATE_PENDING,
+    REFRESH_REQUESTED,
     UPDATED,
-    DELETED
+    DELETED,
+    ;
+
+    private static final Duration NEXT_UPDATABLE_TERM = Duration.ofSeconds(120);
+
+    public boolean isUpdatable(Clock clock, LocalDateTime lastUpdatedAt) {
+        if (UPDATE_NEW == this || REFRESH_NEW == this || REFRESH_REQUESTED == this) {
+            return true;
+        }
+        if (DELETED == this) {
+            return false;
+        }
+        return Duration.between(lastUpdatedAt, LocalDateTime.now(clock)).compareTo(NEXT_UPDATABLE_TERM) > 0;
+    }
 }
