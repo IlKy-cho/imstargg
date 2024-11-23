7a30954669dd2169d4730b0b0806fb874f66a161
Youngrae Cho <yrc9229@gmail.com>
2024-11-23 20:31:06 +0900

    fix(alert-manager): 알람 전송 실패 예외 수정

--- module/alert-manager/src/main/java/com/imstargg/module/alert/DiscordAlertManager.java
+++ module/alert-manager/src/main/java/com/imstargg/module/alert/DiscordAlertManager.java
@@ -2,7 +2,6 @@ package com.imstargg.module.alert;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClient;
 
 class DiscordAlertManager implements AlertManager {
@@ -24,7 +23,7 @@ class DiscordAlertManager implements AlertManager {
                     .uri(webhookUrl)
                     .body(DiscordWebhookRequest.from(alertCommand))
                     .retrieve();
-        } catch (HttpServerErrorException ex) {
+        } catch (Exception ex) {
             log.error("Discord 알람 전송 실패", ex);
         }
     }
