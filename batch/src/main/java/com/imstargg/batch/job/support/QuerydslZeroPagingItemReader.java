62c30510e01942e303bccab7818c2ef6cbfcec6b
Youngrae Cho <yrc9229@gmail.com>
2024-11-24 13:20:27 +0900

    feat(core-collection-db): querydsl itemreader 추가

---
+++ batch/src/main/java/com/imstargg/batch/job/support/QuerydslZeroPagingItemReader.java
@@ -0,0 +1,48 @@
+package com.imstargg.batch.job.support;
+
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.springframework.util.ClassUtils;
+
+import java.util.function.Function;
+
+/**
+ * https://github.com/jojoldu/spring-batch-querydsl
+ */
+public class QuerydslZeroPagingItemReader<T> extends QuerydslPagingItemReader<T> {
+
+    public QuerydslZeroPagingItemReader() {
+        super();
+        setName(ClassUtils.getShortName(QuerydslZeroPagingItemReader.class));
+    }
+
+    public QuerydslZeroPagingItemReader(
+            EntityManagerFactory entityManagerFactory,
+            int pageSize,
+            Function<JPAQueryFactory, JPAQuery<T>> queryFunction) {
+        this();
+        setTransacted(true);
+        super.entityManagerFactory = entityManagerFactory;
+        super.queryFunction = queryFunction;
+        setPageSize(pageSize);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void doReadPage() {
+
+        EntityTransaction tx = getTxOrNull();
+
+        JPQLQuery<T> query = createQuery()
+                .offset(0)
+                .limit(getPageSize());
+
+        initResults();
+
+        fetchQuery(query, tx);
+    }
+
+}
