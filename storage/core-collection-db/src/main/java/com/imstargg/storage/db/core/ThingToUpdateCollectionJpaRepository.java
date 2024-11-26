823fa37f868fbae3c456240338e27d3693ae8d6c
Youngrae Cho <yrc9229@gmail.com>
2024-11-26 15:06:30 +0900

    feat(batch): 업데이트 항목 추가기

--- storage/core-collection-db/src/main/java/com/imstargg/storage/db/core/ThingToUpdateCollectionJpaRepository.java
+++ storage/core-collection-db/src/main/java/com/imstargg/storage/db/core/ThingToUpdateCollectionJpaRepository.java
@@ -2,5 +2,9 @@ package com.imstargg.storage.db.core;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThingToUpdateCollectionJpaRepository extends JpaRepository<ThingToUpdateCollectionEntity, Long> {
+
+    List<ThingToUpdateCollectionEntity> findAllByDeletedFalse();
 }
