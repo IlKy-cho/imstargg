49007d01e3f5803c57d7172f022ecce375aaa49c
Youngrae Cho <yrc9229@gmail.com>
2024-11-26 15:19:53 +0900

    feat(batch): 기존 브롤러 업데이트 잡 제거, 동기화 체크잡 추가

--- batch/src/main/java/com/imstargg/BatchApplication.java
+++ batch/src/main/java/com/imstargg/BatchApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 
 import java.time.Clock;
 import java.time.ZoneId;
 
+@EnableRetry
 @SpringBootApplication
 public class BatchApplication {
 
