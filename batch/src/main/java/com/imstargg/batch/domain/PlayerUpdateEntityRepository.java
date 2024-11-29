b7cbf1c03ce59d013653a940c9c6f56eaf74a8dd
Youngrae Cho <yrc9229@gmail.com>
2024-11-29 14:46:47 +0900

    feat(batch): 업데이트할 플레이어 조회 쿼리

---
+++ batch/src/main/java/com/imstargg/batch/domain/PlayerUpdateEntityRepository.java
@@ -0,0 +1,80 @@
+package com.imstargg.batch.domain;
+
+import com.imstargg.core.enums.PlayerStatus;
+import com.imstargg.storage.db.core.BattleCollectionEntity;
+import com.imstargg.storage.db.core.BattleCollectionJpaRepository;
+import com.imstargg.storage.db.core.PlayerBrawlerCollectionEntity;
+import com.imstargg.storage.db.core.PlayerBrawlerCollectionJpaRepository;
+import com.imstargg.storage.db.core.PlayerCollectionEntity;
+import com.imstargg.storage.db.core.PlayerCollectionJpaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.domain.Limit;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+public class PlayerUpdateEntityRepository {
+
+    private final PlayerCollectionJpaRepository playerCollectionJpaRepository;
+    private final PlayerBrawlerCollectionJpaRepository playerBrawlerCollectionJpaRepository;
+    private final BattleCollectionJpaRepository battleCollectionJpaRepository;
+
+    public PlayerUpdateEntityRepository(
+            PlayerCollectionJpaRepository playerCollectionJpaRepository,
+            PlayerBrawlerCollectionJpaRepository playerBrawlerCollectionJpaRepository,
+            BattleCollectionJpaRepository battleCollectionJpaRepository) {
+        this.playerCollectionJpaRepository = playerCollectionJpaRepository;
+        this.playerBrawlerCollectionJpaRepository = playerBrawlerCollectionJpaRepository;
+        this.battleCollectionJpaRepository = battleCollectionJpaRepository;
+    }
+
+    @Transactional
+    @Retryable(retryFor = OptimisticLockingFailureException.class)
+    public List<PlayerToUpdateEntity> find(int size) {
+        List<PlayerCollectionEntity> players = playerCollectionJpaRepository
+                .findAllWithOptimisticLockByDeletedFalseAndStatusInOrderByUpdateWeight(
+                        List.of(PlayerStatus.UPDATED, PlayerStatus.UPDATING),
+                        Limit.of(size)
+                );
+        players.forEach(player -> player.setStatus(PlayerStatus.UPDATING));
+        List<Long> playerIds = getPlayerIds(players);
+        Map<Long, List<PlayerBrawlerCollectionEntity>> playerIdToBrawlers = getPlayerIdToBrawlers(playerIds);
+        Map<Long, BattleCollectionEntity> playerIdToBattle = getPlayerIdToBattle(playerIds);
+
+        return players.stream()
+                .map(player -> {
+                    List<PlayerBrawlerCollectionEntity> brawlers = playerIdToBrawlers
+                            .getOrDefault(player.getId(), List.of());
+                    Optional<BattleCollectionEntity> battle = Optional.ofNullable(
+                            playerIdToBattle.get(player.getId()));
+                    return new PlayerToUpdateEntity(player, brawlers, battle);
+                })
+                .toList();
+    }
+
+    private static List<Long> getPlayerIds(List<PlayerCollectionEntity> players) {
+        return players.stream()
+                .map(PlayerCollectionEntity::getId)
+                .toList();
+    }
+
+    private Map<Long, List<PlayerBrawlerCollectionEntity>> getPlayerIdToBrawlers(List<Long> playerIds) {
+        return playerBrawlerCollectionJpaRepository
+                .findAllByPlayerIdIn(playerIds)
+                .stream()
+                .collect(Collectors.groupingBy(PlayerBrawlerCollectionEntity::getPlayerId));
+    }
+
+    private Map<Long, BattleCollectionEntity> getPlayerIdToBattle(List<Long> playerIds) {
+        return battleCollectionJpaRepository
+                .findAllLastBattleByPlayerIdIn(playerIds)
+                .stream()
+                .collect(Collectors.toMap(battle -> battle.getPlayer().getPlayerId(), battle -> battle));
+    }
+}
---
+++ batch/src/test/java/com/imstargg/batch/domain/PlayerUpdateEntityRepositoryTest.java
@@ -0,0 +1,79 @@
+package com.imstargg.batch.domain;
+
+import com.imstargg.core.enums.PlayerStatus;
+import com.imstargg.storage.db.core.BattleCollectionEntity;
+import com.imstargg.storage.db.core.BattleCollectionEntityPlayer;
+import com.imstargg.storage.db.core.BattleCollectionJpaRepository;
+import com.imstargg.storage.db.core.PlayerBrawlerCollectionEntity;
+import com.imstargg.storage.db.core.PlayerBrawlerCollectionJpaRepository;
+import com.imstargg.storage.db.core.PlayerCollectionEntity;
+import com.imstargg.storage.db.core.PlayerCollectionJpaRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Limit;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class PlayerUpdateEntityRepositoryTest {
+
+    @InjectMocks
+    private PlayerUpdateEntityRepository playerUpdateEntityRepository;
+    @Mock
+    private PlayerCollectionJpaRepository playerCollectionJpaRepository;
+    @Mock
+    private PlayerBrawlerCollectionJpaRepository playerBrawlerCollectionJpaRepository;
+    @Mock
+    private BattleCollectionJpaRepository battleCollectionJpaRepository;
+
+    @Test
+    void 업데이트할_플레이어를_조회한다() {
+        // given
+        int size = 10;
+        PlayerCollectionEntity player1 = mock(PlayerCollectionEntity.class);
+        given(player1.getId()).willReturn(1L);
+        PlayerCollectionEntity player2 = mock(PlayerCollectionEntity.class);
+        given(player2.getId()).willReturn(2L);
+        given(playerCollectionJpaRepository
+                .findAllWithOptimisticLockByDeletedFalseAndStatusInOrderByUpdateWeight(
+                        List.of(PlayerStatus.UPDATED, PlayerStatus.UPDATING),
+                        Limit.of(size)
+                )).willReturn(List.of(player1, player2));
+
+        PlayerBrawlerCollectionEntity player1Brawler1 = mock(PlayerBrawlerCollectionEntity.class);
+        PlayerBrawlerCollectionEntity player1Brawler2 = mock(PlayerBrawlerCollectionEntity.class);
+        given(player1Brawler1.getPlayerId()).willReturn(1L);
+        given(player1Brawler2.getPlayerId()).willReturn(1L);
+        given(playerBrawlerCollectionJpaRepository.findAllByPlayerIdIn(List.of(1L, 2L))).willReturn(List.of(
+                player1Brawler1, player1Brawler2
+        ));
+
+        BattleCollectionEntity battle1 = mock(BattleCollectionEntity.class);
+        BattleCollectionEntityPlayer battle1Player1 = mock(BattleCollectionEntityPlayer.class);
+        given(battle1Player1.getPlayerId()).willReturn(1L);
+        given(battle1.getPlayer()).willReturn(battle1Player1);
+        given(battleCollectionJpaRepository.findAllLastBattleByPlayerIdIn(List.of(1L, 2L))).willReturn(List.of(
+                battle1
+        ));
+
+        // when
+        List<PlayerToUpdateEntity> playerToUpdateEntities = playerUpdateEntityRepository.find(size);
+
+        // then
+        assertThat(playerToUpdateEntities).hasSize(2);
+        assertThat(playerToUpdateEntities.get(0).playerEntity().getId()).isEqualTo(1L);
+        assertThat(playerToUpdateEntities.get(0).playerBrawlerEntities()).hasSize(2);
+        assertThat(playerToUpdateEntities.get(0).lastBattleEntity()).isPresent();
+
+        assertThat(playerToUpdateEntities.get(1).playerEntity().getId()).isEqualTo(2L);
+        assertThat(playerToUpdateEntities.get(1).playerBrawlerEntities()).isEmpty();
+        assertThat(playerToUpdateEntities.get(1).lastBattleEntity()).isEmpty();
+    }
+}
