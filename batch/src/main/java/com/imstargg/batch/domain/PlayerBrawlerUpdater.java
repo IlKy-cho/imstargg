cf42d172f3818fb7d20904abfa5ead5cb368d261
Youngrae Cho <yrc9229@gmail.com>
2024-11-29 10:53:40 +0900

    feat(batch): 플레이어 브롤러 업데이트 로직

---
+++ batch/src/main/java/com/imstargg/batch/domain/PlayerBrawlerUpdater.java
@@ -0,0 +1,54 @@
+package com.imstargg.batch.domain;
+
+import com.imstargg.client.brawlstars.response.AccessoryResponse;
+import com.imstargg.client.brawlstars.response.BrawlerStatResponse;
+import com.imstargg.client.brawlstars.response.GearStatResponse;
+import com.imstargg.client.brawlstars.response.StarPowerResponse;
+import com.imstargg.storage.db.core.PlayerBrawlerCollectionEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class PlayerBrawlerUpdater {
+
+    public List<PlayerBrawlerCollectionEntity> update(
+            long playerId,
+            List<PlayerBrawlerCollectionEntity> playerBrawlerEntities,
+            List<BrawlerStatResponse> brawlerResponseList
+    ) {
+        Map<Long, PlayerBrawlerCollectionEntity> idToBrawler = playerBrawlerEntities.stream()
+                .collect(Collectors.toMap(PlayerBrawlerCollectionEntity::getBrawlerBrawlStarsId, Function.identity()));
+
+        for (BrawlerStatResponse brawlerResponse : brawlerResponseList) {
+            PlayerBrawlerCollectionEntity brawler = idToBrawler.computeIfAbsent(brawlerResponse.id(), id ->
+                    new PlayerBrawlerCollectionEntity(
+                            playerId,
+                            brawlerResponse.id(),
+                            brawlerResponse.power(),
+                            brawlerResponse.rank(),
+                            brawlerResponse.trophies(),
+                            brawlerResponse.highestTrophies(),
+                            brawlerResponse.gears().stream().map(GearStatResponse::id).toList(),
+                            brawlerResponse.starPowers().stream().map(StarPowerResponse::id).toList(),
+                            brawlerResponse.gadgets().stream().map(AccessoryResponse::id).toList()
+                    ));
+
+            brawler.setPower(brawlerResponse.power());
+            brawler.setRank(brawlerResponse.rank());
+            brawler.setTrophies(brawlerResponse.trophies());
+            brawler.setHighestTrophies(brawlerResponse.highestTrophies());
+            brawler.setGearBrawlStarsIds(
+                    brawlerResponse.gears().stream().map(GearStatResponse::id).toList());
+            brawler.setStarPowerBrawlStarsIds(
+                    brawlerResponse.starPowers().stream().map(StarPowerResponse::id).toList());
+            brawler.setGadgetBrawlStarsIds(
+                    brawlerResponse.gadgets().stream().map(AccessoryResponse::id).toList());
+        }
+
+        return idToBrawler.values().stream().toList();
+    }
+}
---
+++ batch/src/test/java/com/imstargg/batch/domain/PlayerBrawlerUpdaterTest.java
@@ -0,0 +1,128 @@
+package com.imstargg.batch.domain;
+
+import com.imstargg.client.brawlstars.response.BrawlerStatResponse;
+import com.imstargg.client.brawlstars.response.GearStatResponse;
+import com.imstargg.client.brawlstars.response.StarPowerResponse;
+import com.imstargg.core.enums.Brawler;
+import com.imstargg.storage.db.core.PlayerBrawlerCollectionEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayerBrawlerUpdaterTest {
+
+    private PlayerBrawlerUpdater playerBrawlerUpdater;
+
+    @BeforeEach
+    void setUp() {
+        playerBrawlerUpdater = new PlayerBrawlerUpdater();
+    }
+
+    @Test
+    void 브롤러를_업데이트한다() {
+        // given
+        long playerId = 123;
+        List<PlayerBrawlerCollectionEntity> playerBrawlerEntities = List.of(
+                new PlayerBrawlerCollectionEntity(
+                        playerId,
+                        16000000,
+                        8,
+                        23,
+                        449,
+                        453,
+                        List.of(),
+                        List.of(),
+                        List.of(Brawler.SHELLY.getGadgets().get(0).getBrawlStarsId())
+                )
+        );
+        List<BrawlerStatResponse> brawlerResponseList = List.of(
+                new BrawlerStatResponse(
+                        Brawler.SHELLY.getBrawlStarsId(),
+                        Brawler.SHELLY.getBrawlStarsName(),
+                        11,
+                        25,
+                        500,
+                        500,
+                        List.of(
+                                new GearStatResponse(
+                                        Brawler.SHELLY.getGears().get(0).getBrawlStarsId(),
+                                        Brawler.SHELLY.getGears().get(0).getBrawlStarsName(),
+                                        Brawler.SHELLY.getGears().get(0).getLevel()
+
+                                )
+                        ),
+                        List.of(),
+                        List.of(
+                                new StarPowerResponse(
+                                        Brawler.SHELLY.getStarPowers().get(0).getBrawlStarsId(),
+                                        Brawler.SHELLY.getStarPowers().get(0).getBrawlStarsName()
+                                ),
+                                new StarPowerResponse(
+                                        Brawler.SHELLY.getStarPowers().get(1).getBrawlStarsId(),
+                                        Brawler.SHELLY.getStarPowers().get(1).getBrawlStarsName()
+                                )
+                        )
+                ),
+                new BrawlerStatResponse(
+                        Brawler.NITA.getBrawlStarsId(),
+                        Brawler.NITA.getBrawlStarsName(),
+                        1,
+                        1,
+                        0,
+                        0,
+                        List.of(),
+                        List.of(),
+                        List.of()
+                )
+        );
+
+        // when
+        List<PlayerBrawlerCollectionEntity> updatedEntities = playerBrawlerUpdater
+                .update(playerId, playerBrawlerEntities, brawlerResponseList);
+
+        // then
+        assertThat(updatedEntities)
+                .describedAs("기존에 없던 브롤러는 새로 추가되어야 한다.")
+                .hasSize(2);
+        assertThat(updatedEntities.get(0))
+                .describedAs("기존 브롤러는 수정되어야 한다.")
+                .isEqualTo(playerBrawlerEntities.get(0));
+        assertThat(updatedEntities.get(0).getBrawlerBrawlStarsId()).isEqualTo(Brawler.SHELLY.getBrawlStarsId());
+        assertThat(updatedEntities.get(0).getPower()).isEqualTo(11);
+        assertThat(updatedEntities.get(0).getRank()).isEqualTo(25);
+        assertThat(updatedEntities.get(0).getTrophies()).isEqualTo(500);
+        assertThat(updatedEntities.get(0).getHighestTrophies()).isEqualTo(500);
+        assertThat(updatedEntities.get(0).getGearBrawlStarsIds())
+                .describedAs("빈 리스트에 새로 추가될 수 있어야 한다.")
+                .hasSize(1);
+        assertThat(updatedEntities.get(0).getGearBrawlStarsIds().get(0))
+                .isEqualTo(Brawler.SHELLY.getGears().get(0).getBrawlStarsId());
+        assertThat(updatedEntities.get(0).getStarPowerBrawlStarsIds())
+                .describedAs("비어있지 않은 리스트에 새로 추가될 수 있어야 한다.")
+                .hasSize(2);
+        assertThat(updatedEntities.get(0).getStarPowerBrawlStarsIds().get(0))
+                .isEqualTo(Brawler.SHELLY.getStarPowers().get(0).getBrawlStarsId());
+        assertThat(updatedEntities.get(0).getStarPowerBrawlStarsIds().get(1))
+                .isEqualTo(Brawler.SHELLY.getStarPowers().get(1).getBrawlStarsId());
+        assertThat(updatedEntities.get(0).getGadgetBrawlStarsIds())
+                .describedAs("변경사항이 없을 수 있다.")
+                .isEmpty();
+        assertThat(updatedEntities.get(1).getId())
+                .describedAs("추가된 브롤러는 생성되어야 한다.")
+                .isNull();
+        assertThat(updatedEntities.get(1).getPlayerId())
+                .describedAs("브롤러가 추가될 때는 인자로 넘겨준 playerId를 그대로 사용해야 한다.")
+                .isEqualTo(playerId);
+        assertThat(updatedEntities.get(1).getBrawlerBrawlStarsId()).isEqualTo(Brawler.NITA.getBrawlStarsId());
+        assertThat(updatedEntities.get(1).getPower()).isEqualTo(1);
+        assertThat(updatedEntities.get(1).getRank()).isEqualTo(1);
+        assertThat(updatedEntities.get(1).getTrophies()).isZero();
+        assertThat(updatedEntities.get(1).getHighestTrophies()).isZero();
+        assertThat(updatedEntities.get(1).getGearBrawlStarsIds()).isEmpty();
+        assertThat(updatedEntities.get(1).getStarPowerBrawlStarsIds()).isEmpty();
+        assertThat(updatedEntities.get(1).getGadgetBrawlStarsIds()).isEmpty();
+    }
+}
