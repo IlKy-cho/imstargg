578d157768ad4707fd6a01175c89fa70a5f32bc2
Youngrae Cho <yrc9229@gmail.com>
2024-11-29 14:46:47 +0900

    feat(batch): 업데이트할 플레이어 조회 쿼리

---
+++ batch/src/main/java/com/imstargg/batch/domain/PlayerToUpdateEntity.java
@@ -0,0 +1,15 @@
+package com.imstargg.batch.domain;
+
+import com.imstargg.storage.db.core.BattleCollectionEntity;
+import com.imstargg.storage.db.core.PlayerBrawlerCollectionEntity;
+import com.imstargg.storage.db.core.PlayerCollectionEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public record PlayerToUpdateEntity(
+        PlayerCollectionEntity playerEntity,
+        List<PlayerBrawlerCollectionEntity> playerBrawlerEntities,
+        Optional<BattleCollectionEntity> lastBattleEntity
+) {
+}
--- storage/core-collection-db/src/main/java/com/imstargg/storage/db/core/PlayerCollectionJpaRepository.java
+++ storage/core-collection-db/src/main/java/com/imstargg/storage/db/core/PlayerCollectionJpaRepository.java
@@ -1,14 +1,17 @@
 package com.imstargg.storage.db.core;
 
 import com.imstargg.core.enums.PlayerStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface PlayerCollectionJpaRepository extends JpaRepository<PlayerCollectionEntity, Long> {
 
-    List<PlayerCollectionEntity> findAllByDeletedFalseAndStatusInOrderByUpdateWeight(
-            Collection<PlayerStatus> status, Limit limit);
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    List<PlayerCollectionEntity> findAllWithOptimisticLockByDeletedFalseAndStatusInOrderByUpdateWeight(
+            Collection<PlayerStatus> statuses, Limit limit);
 }
