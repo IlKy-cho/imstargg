6283b29d6afdbd5227173c783a72be17dfc13c12
Youngrae Cho <yrc9229@gmail.com>
2024-11-29 10:23:09 +0900

    feat(core-collection-db): 플레이어 엔티티 생성, 업데이트 가중치 업데이트 추가

--- core/core-enum/src/main/java/com/imstargg/core/enums/PlayerStatus.java
+++ core/core-enum/src/main/java/com/imstargg/core/enums/PlayerStatus.java
@@ -3,8 +3,6 @@ package com.imstargg.core.enums;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.List;
 
 public enum PlayerStatus {
 
@@ -21,28 +19,5 @@ public enum PlayerStatus {
             return false;
         }
         return Duration.between(lastUpdatedAt, LocalDateTime.now(clock)).compareTo(NEXT_UPDATABLE_TERM) > 0;
-    }
-
-    public long nextUpdateWeight(
-            Clock clock,
-            List<PlayTime> playTimes,
-            int expLevel,
-            int playerTrophies,
-            int brawlerTrophies
-    ) {
-        // TODO
-        LocalDateTime now = LocalDateTime.now(clock);
-        long timeDiffAvg = playTimes.stream()
-                .map(PlayTime::battleTime)
-                .mapToLong(battleTime -> Duration.between(battleTime, now).getSeconds())
-                .sum() / playTimes.size();
-        long durationAvg = playTimes.stream()
-                .mapToLong(PlayTime::duration)
-                .sum() / playTimes.size();
-
-        return now.plusHours(1).toInstant(ZoneOffset.UTC).toEpochMilli();
-    }
-
-    public record PlayTime(LocalDateTime battleTime, int duration) {
     }
 }
---
+++ storage/core-collection-db/src/testFixtures/java/com/imstargg/storage/db/core/PlayerCollectionEntityFixture.java
@@ -0,0 +1,182 @@
+package com.imstargg.storage.db.core;
+
+import com.imstargg.core.enums.PlayerStatus;
+import com.imstargg.test.java.LongIncrementUtil;
+import jakarta.annotation.Nullable;
+
+import java.time.LocalDateTime;
+
+public class PlayerCollectionEntityFixture {
+
+    @Nullable
+    private String brawlStarsTag;
+
+    @Nullable
+    private PlayerStatus status;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private String nameColor;
+
+    private long iconBrawlStarsId;
+
+    private int trophies;
+
+    private int highestTrophies;
+
+    private int expLevel;
+
+    private int expPoints;
+
+    private boolean qualifiedFromChampionshipChallenge;
+
+    private int victories3vs3;
+
+    private int soloVictories;
+
+    private int duoVictories;
+
+    private int bestRoboRumbleTime;
+
+    private int bestTimeAsBigBrawler;
+
+    @Nullable
+    private String brawlStarsClubTag;
+
+    private int notUpdatedCount;
+
+    private long updateWeight;
+
+    public PlayerCollectionEntityFixture brawlStarsTag(String brawlStarsTag) {
+        this.brawlStarsTag = brawlStarsTag;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture status(PlayerStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture nameColor(String nameColor) {
+        this.nameColor = nameColor;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture iconBrawlStarsId(long iconBrawlStarsId) {
+        this.iconBrawlStarsId = iconBrawlStarsId;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture trophies(int trophies) {
+        this.trophies = trophies;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture highestTrophies(int highestTrophies) {
+        this.highestTrophies = highestTrophies;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture expLevel(int expLevel) {
+        this.expLevel = expLevel;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture expPoints(int expPoints) {
+        this.expPoints = expPoints;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture qualifiedFromChampionshipChallenge(boolean qualifiedFromChampionshipChallenge) {
+        this.qualifiedFromChampionshipChallenge = qualifiedFromChampionshipChallenge;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture victories3vs3(int victories3vs3) {
+        this.victories3vs3 = victories3vs3;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture soloVictories(int soloVictories) {
+        this.soloVictories = soloVictories;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture duoVictories(int duoVictories) {
+        this.duoVictories = duoVictories;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture bestRoboRumbleTime(int bestRoboRumbleTime) {
+        this.bestRoboRumbleTime = bestRoboRumbleTime;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture bestTimeAsBigBrawler(int bestTimeAsBigBrawler) {
+        this.bestTimeAsBigBrawler = bestTimeAsBigBrawler;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture brawlStarsClubTag(String brawlStarsClubTag) {
+        this.brawlStarsClubTag = brawlStarsClubTag;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture notUpdatedCount(int notUpdatedCount) {
+        this.notUpdatedCount = notUpdatedCount;
+        return this;
+    }
+
+    public PlayerCollectionEntityFixture updateWeight(long updateWeight) {
+        this.updateWeight = updateWeight;
+        return this;
+    }
+
+    public PlayerCollectionEntity build() {
+        fillRequiredFields();
+        return new PlayerCollectionEntity(
+            status,
+            brawlStarsTag,
+            name,
+            nameColor,
+            iconBrawlStarsId,
+            trophies,
+            highestTrophies,
+            expLevel,
+            expPoints,
+            qualifiedFromChampionshipChallenge,
+            victories3vs3,
+            soloVictories,
+            duoVictories,
+            bestRoboRumbleTime,
+            bestTimeAsBigBrawler,
+            brawlStarsClubTag,
+            LocalDateTime.now()
+        );
+    }
+
+    private void fillRequiredFields() {
+        if (status == null) {
+            status = PlayerStatus.UPDATING;
+        }
+        if (brawlStarsTag == null) {
+            brawlStarsTag = "brawlStarsTag-" + LongIncrementUtil.next();
+        }
+        if (name == null) {
+            name = "name-" + LongIncrementUtil.next();
+        }
+        if (nameColor == null) {
+            nameColor = "nameColor-" + LongIncrementUtil.next();
+        }
+        if (brawlStarsClubTag == null) {
+            brawlStarsClubTag = "brawlStarsClubTag-" + LongIncrementUtil.next();
+        }
+    }
+}
