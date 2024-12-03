490a195672f20a7ba5e24d1fc4fee400f502c3a7
Youngrae Cho <yrc9229@gmail.com>
2024-12-03 15:27:43 +0900

    feat(core): 불필요한 개념클래스 제거

--- core/core-domain/src/main/java/com/imstargg/core/domain/Gadget.java
+++
@@ -1,7 +0,0 @@
-package com.imstargg.core.domain;
-
-public record Gadget(
-        BrawlStarsId id,
-        String name
-) {
-}
--- core/core-domain/src/main/java/com/imstargg/core/domain/PlayerBrawler.java
+++ core/core-domain/src/main/java/com/imstargg/core/domain/PlayerBrawler.java
@@ -1,5 +1,10 @@
 package com.imstargg.core.domain;
 
+import com.imstargg.core.enums.Brawler;
+import com.imstargg.core.enums.Gadget;
+import com.imstargg.core.enums.Gear;
+import com.imstargg.core.enums.StarPower;
+
 import java.util.List;
 
 public record PlayerBrawler(
