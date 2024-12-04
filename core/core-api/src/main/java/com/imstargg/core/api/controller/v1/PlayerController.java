eb196c1bbc6825373cb6382612ad0fa68939497b
Youngrae Cho <yrc9229@gmail.com>
2024-12-04 13:43:22 +0900

    feat(core): 플레이어 조회 API 추가

--- core/core-api/src/main/java/com/imstargg/core/api/controller/v1/PlayerController.java
+++ core/core-api/src/main/java/com/imstargg/core/api/controller/v1/PlayerController.java
@@ -1,21 +1,27 @@
 package com.imstargg.core.api.controller.v1;
 
 import com.imstargg.core.api.controller.v1.request.PlayerSearchRequest;
+import com.imstargg.core.api.controller.v1.response.PlayerResponse;
 import com.imstargg.core.api.controller.v1.response.PlayerSearchResponse;
+import com.imstargg.core.domain.BrawlStarsTag;
 import com.imstargg.core.domain.PlayerSearchService;
+import com.imstargg.core.domain.PlayerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlayerController {
 
     private final PlayerSearchService playerSearchService;
+    private final PlayerService playerService;
 
-    public PlayerController(PlayerSearchService playerSearchService) {
+    public PlayerController(PlayerSearchService playerSearchService, PlayerService playerService) {
         this.playerSearchService = playerSearchService;
+        this.playerService = playerService;
     }
 
     @GetMapping("/api/v1/search")
@@ -25,5 +31,10 @@ public class PlayerController {
                 .from(playerSearchService.search(request.toParam()));
         return ResponseEntity.accepted().body(response);
 
+    }
+
+    @GetMapping("/api/v1/players/{tag}")
+    public PlayerResponse get(@PathVariable String tag) {
+        return PlayerResponse.from(playerService.get(new BrawlStarsTag(tag)));
     }
 }
