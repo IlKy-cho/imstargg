e3fbd763d4acfbc3ace6f660e4e7dba94e0c506e
Youngrae Cho <yrc9229@gmail.com>
2024-12-04 12:45:15 +0900

    feat(core): 새 플레이어 새로고침 요청 로직

---
+++ core/core-domain/src/main/java/com/imstargg/core/domain/PlayerRefreshEventPublisher.java
@@ -0,0 +1,11 @@
+package com.imstargg.core.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlayerRefreshEventPublisher {
+
+    public void publishNew(BrawlStarsTag tag) {
+        // TODO
+    }
+}
