481f0148fdb6fd9104a6801844cc9605cfe526e6
Youngrae Cho <yrc9229@gmail.com>
2024-12-04 13:10:08 +0900

    test(core): 테스트 깨짐 수정

--- core/core-api/src/test/java/com/imstargg/CoreApiApplicationTest.java
+++ core/core-api/src/test/java/com/imstargg/CoreApiApplicationTest.java
@@ -1,14 +1,12 @@
-package com.imstargg.core.domain;
+package com.imstargg;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
 @SpringBootTest
 @Tag("context")
-class CoreDomainTest {
+class CoreApiApplicationTest {
 
     @Test
     void contextLoads() {
