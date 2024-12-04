0074aaa4397ddda0f35383bb9eb1ab2abd788593
Youngrae Cho <yrc9229@gmail.com>
2024-12-04 22:59:09 +0900

    feat(core-db): 플레이어 배틀 업데이트 후 상태 변경 로직 수정

---
+++ storage/core-collection-db/src/test/java/com/imstargg/storage/db/core/PlayerCollectionEntityContextTest.java
@@ -0,0 +1,162 @@
+package com.imstargg.storage.db.core;
+
+import com.imstargg.core.enums.Brawler;
+import com.imstargg.core.enums.PlayerStatus;
+import com.imstargg.storage.db.core.test.AbstractDataJpaTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Tag("context")
+class PlayerCollectionEntityContextTest extends AbstractDataJpaTest {
+
+    @Autowired
+    private PlayerCollectionJpaRepository repository;
+
+    @Test
+    void 생성하면_notUpdatedCount는_0_updateWeight는_now로_초기화되고_정상적으로_저장된다() {
+        // given
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        PlayerCollectionEntity entity = new PlayerCollectionEntity(
+                "brawlStarsTag",
+                "name",
+                "nameColor",
+                1,
+                1000,
+                2000,
+                10,
+                5000,
+                false,
+                50,
+                20,
+                30,
+                120,
+                60,
+                "brawlStarsClubTag",
+                LocalDateTime.now(clock)
+        );
+
+        // when
+        repository.save(entity);
+
+        // then
+        PlayerCollectionEntity savedEntity = repository.findById(entity.getId()).get();
+        assertThat(savedEntity.getNotUpdatedCount()).isZero();
+        assertThat(savedEntity.getUpdateWeight())
+                .isEqualTo(LocalDateTime.now(clock));
+        assertThat(savedEntity.getStatus()).isEqualTo(PlayerStatus.NEW);
+        assertThat(savedEntity.getBrawlStarsTag()).isEqualTo("brawlStarsTag");
+        assertThat(savedEntity.getName()).isEqualTo("name");
+        assertThat(savedEntity.getNameColor()).isEqualTo("nameColor");
+        assertThat(savedEntity.getIconBrawlStarsId()).isEqualTo(1);
+        assertThat(savedEntity.getTrophies()).isEqualTo(1000);
+        assertThat(savedEntity.getHighestTrophies()).isEqualTo(2000);
+        assertThat(savedEntity.getExpLevel()).isEqualTo(10);
+        assertThat(savedEntity.getExpPoints()).isEqualTo(5000);
+        assertThat(savedEntity.isQualifiedFromChampionshipChallenge()).isFalse();
+        assertThat(savedEntity.getVictories3vs3()).isEqualTo(50);
+        assertThat(savedEntity.getSoloVictories()).isEqualTo(20);
+        assertThat(savedEntity.getDuoVictories()).isEqualTo(30);
+        assertThat(savedEntity.getBestRoboRumbleTime()).isEqualTo(120);
+        assertThat(savedEntity.getBestTimeAsBigBrawler()).isEqualTo(60);
+        assertThat(savedEntity.getBrawlStarsClubTag()).isEqualTo("brawlStarsClubTag");
+    }
+
+    @Test
+    void 브롤러를_업데이트할_수_있다() {
+        // given
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        PlayerCollectionEntity player = new PlayerCollectionEntity(
+                "brawlStarsTag",
+                "name",
+                "nameColor",
+                1,
+                1000,
+                2000,
+                10,
+                5000,
+                false,
+                50,
+                20,
+                30,
+                120,
+                60,
+                "brawlStarsClubTag",
+                LocalDateTime.now(clock)
+        );
+        player.updateBrawler(
+                Brawler.ASH.getBrawlStarsId(),
+                1,
+                1,
+                0,
+                0,
+                List.of(),
+                List.of(),
+                List.of()
+        );
+        repository.save(player);
+
+        em.flush();
+        em.clear();
+
+        // when
+        PlayerCollectionEntity foundPlayer = repository.findById(player.getId()).get();
+        foundPlayer.updateBrawler(
+                Brawler.ASH.getBrawlStarsId(),
+                2,
+                2,
+                100,
+                200,
+                List.of(1L, 2L),
+                List.of(3L, 4L),
+                List.of(5L, 6L)
+        );
+        foundPlayer.updateBrawler(
+                Brawler.BROCK.getBrawlStarsId(),
+                3,
+                3,
+                300,
+                400,
+                List.of(7L, 8L),
+                List.of(9L, 10L),
+                List.of(11L, 12L)
+        );
+        repository.save(foundPlayer);
+
+        em.flush();
+        em.clear();
+
+        // then
+        PlayerCollectionEntity updatedPlayer = repository.findById(foundPlayer.getId()).get();
+        assertThat(updatedPlayer.getBrawlers())
+                .hasSize(2)
+                .anySatisfy(playerBrawler -> {
+                    assertThat(playerBrawler.getBrawlerBrawlStarsId()).isEqualTo(Brawler.ASH.getBrawlStarsId());
+                    assertThat(playerBrawler.getPower()).isEqualTo(2);
+                    assertThat(playerBrawler.getRank()).isEqualTo(2);
+                    assertThat(playerBrawler.getTrophies()).isEqualTo(100);
+                    assertThat(playerBrawler.getHighestTrophies()).isEqualTo(200);
+                    assertThat(playerBrawler.getGearBrawlStarsIds()).containsExactly(1L, 2L);
+                    assertThat(playerBrawler.getStarPowerBrawlStarsIds()).containsExactly(3L, 4L);
+                    assertThat(playerBrawler.getGadgetBrawlStarsIds()).containsExactly(5L, 6L);
+                })
+                .anySatisfy(playerBrawler -> {
+                    assertThat(playerBrawler.getBrawlerBrawlStarsId()).isEqualTo(Brawler.BROCK.getBrawlStarsId());
+                    assertThat(playerBrawler.getPower()).isEqualTo(3);
+                    assertThat(playerBrawler.getRank()).isEqualTo(3);
+                    assertThat(playerBrawler.getTrophies()).isEqualTo(300);
+                    assertThat(playerBrawler.getHighestTrophies()).isEqualTo(400);
+                    assertThat(playerBrawler.getGearBrawlStarsIds()).containsExactly(7L, 8L);
+                    assertThat(playerBrawler.getStarPowerBrawlStarsIds()).containsExactly(9L, 10L);
+                    assertThat(playerBrawler.getGadgetBrawlStarsIds()).containsExactly(11L, 12L);
+                });
+    }
+}
