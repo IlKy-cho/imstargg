11f2ca2e9f5ee77300013634a454b4743f4d91ef
Youngrae Cho <yrc9229@gmail.com>
2024-12-06 19:02:50 +0900

    feat(frontend): 광고 공간 확보

---
+++ frontend/imstargg-frontend/src/app/_component/ads/BottomAd.tsx
@@ -0,0 +1,9 @@
+export default function BottomAd() {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 w-full h-[100px] bg-gray-50/80 backdrop-blur-sm">
+      <div className="container mx-auto p-2">
+        {/* 광고 내용 */}ㄴㅁㄹㅇㅁㄴㅇㄹ
+      </div>
+    </div>
+  );
+} 
