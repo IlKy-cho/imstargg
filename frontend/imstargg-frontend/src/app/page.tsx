a06641f02a4139291ab11dd8dd9e39a68af201cb
Youngrae Cho <yrc9229@gmail.com>
2024-12-06 19:49:40 +0900

    feat(front): 플레이어 검색

--- frontend/imstargg-frontend/src/app/page.tsx
+++ frontend/imstargg-frontend/src/app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import PlayerSearch from "./_component/PlayerSearch";
 
 export default function Home() {
   return (
-    <div>
-      홈화면
-    </div>
+    <main className="flex flex-col items-center justify-center p-24">
+      <div className="relative mb-8">
+        <Image
+          src="/logo.png"
+          alt="ImStarGG 로고"
+          width={200}
+          height={100}
+          priority
+        />
+      </div>
+      
+      <PlayerSearch />
+    </main>
   );
 }
