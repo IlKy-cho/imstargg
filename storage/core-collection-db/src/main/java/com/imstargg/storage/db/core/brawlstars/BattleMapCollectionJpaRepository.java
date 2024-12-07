d6a390768da26f274b7dafa99c7be43b29c7c373
Youngrae Cho <yrc9229@gmail.com>
2024-12-07 12:26:33 +0900

    feat(core-db): 메타데이터 계속 디비로 관리하기로 결정

--- storage/core-collection-db/src/main/java/com/imstargg/storage/db/core/brawlstars/BattleMapCollectionJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.imstargg.storage.db.core.brawlstars;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BattleMapCollectionJpaRepository extends JpaRepository<BattleMapCollectionEntity, Long> {
-} 
--- storage/core-collection-db/src/main/java/com/imstargg/storage/db/core/brawlstars/GearCollectionJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.imstargg.storage.db.core.brawlstars;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GearCollectionJpaRepository extends JpaRepository<GearCollectionEntity, Long> {
-} 
