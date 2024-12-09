293272583a0a0131a4194b8c763d9118200c5773
Youngrae Cho <yrc9229@gmail.com>
2024-12-09 14:41:26 +0900

    feat(admin): 브롤러 등록 API 추가

---
+++ admin/src/main/java/com/imstargg/admin/controller/request/NewBrawlerRequest.java
@@ -0,0 +1,22 @@
+package com.imstargg.admin.controller.request;
+
+import com.imstargg.admin.domain.NewBrawler;
+import com.imstargg.core.enums.BrawlerRarity;
+import com.imstargg.core.enums.BrawlerRole;
+import com.imstargg.core.enums.Language;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.EnumMap;
+
+public record NewBrawlerRequest(
+        long brawlStarsId,
+        @NotNull BrawlerRarity rarity,
+        @NotNull BrawlerRole role,
+        @NotNull @NotEmpty EnumMap<Language, String> names
+) {
+
+    public NewBrawler toNewBrawler() {
+        return new NewBrawler(brawlStarsId, rarity, role, names);
+    }
+}
