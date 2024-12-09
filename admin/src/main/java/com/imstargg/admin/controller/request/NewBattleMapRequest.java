3455545a3474994a42dc29d19a61dbf05fb79a36
Youngrae Cho <yrc9229@gmail.com>
2024-12-09 21:16:32 +0900

    feat(admin): 맵 등록 API 추가

--- admin/src/main/java/com/imstargg/admin/controller/request/NewBattleMapRequest.java
+++ admin/src/main/java/com/imstargg/admin/controller/request/NewBattleMapRequest.java
@@ -1,17 +1,18 @@
 package com.imstargg.admin.controller.request;
 
-import com.imstargg.admin.domain.BrawlerUpdate;
 import com.imstargg.admin.domain.MessageCollection;
+import com.imstargg.admin.domain.NewBattleMap;
 import com.imstargg.core.enums.Language;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.EnumMap;
 
-public record BrawlerUpdateRequest(
+public record NewBattleMapRequest(
+        @NotBlank String code,
         @NotEmpty EnumMap<Language, String> names
 ) {
-
-    public BrawlerUpdate toBrawlerUpdate() {
-        return new BrawlerUpdate(new MessageCollection(names));
+    public NewBattleMap toNewBattleMap() {
+        return new NewBattleMap(code, new MessageCollection(names));
     }
 }
