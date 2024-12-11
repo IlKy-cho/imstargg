3a3f66fbbbf4f66a38cac8fb2c7e8c44d534514c
Youngrae Cho <yrc9229@gmail.com>
2024-12-11 16:15:27 +0900

    feat(front): 모델 추가 =

---
+++ frontend/imstargg-frontend/src/model/PlayerBattle.ts
@@ -0,0 +1,16 @@
+import BattlePlayer from "@/model/BattlePlayer";
+import {BattleResult} from "@/model/BattleResult";
+import {BattleType} from "@/model/BattleType";
+import BattleEvent from "@/model/BattleEvent";
+
+export default interface PlayerBattle {
+  battleTime: Date;
+  event: BattleEvent;
+  type: BattleType;
+  result: BattleResult | null;
+  duration: number | null;
+  rank: number | null;
+  trophyChange: number | null;
+  starPlayerTag: string | null;
+  teams: BattlePlayer[][];
+}
---
+++ frontend/imstargg-frontend/src/model/StarPower.ts
@@ -0,0 +1,4 @@
+export default interface StarPower {
+  id: number;
+  name: string;
+}
