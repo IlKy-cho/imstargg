36b766a2963cfe54077ad3b2464931a7621e0b2b
Youngrae Cho <yrc9229@gmail.com>
2024-12-15 09:03:16 +0900

    feat(brawlstars-client): 클라이언트 응답 캐싱

---
+++ client/brawlstars-client/src/main/java/com/imstargg/client/brawlstars/BrawlStarsClientCacheConfig.java
@@ -0,0 +1,28 @@
+package com.imstargg.client.brawlstars;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+class BrawlStarsClientCacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(Duration.ofSeconds(15))
+                        .maximumSize(1000)
+        );
+        cacheManager.setCacheNames(List.of(
+                "brawlstars-client"
+        ));
+        return cacheManager;
+    }
+}
