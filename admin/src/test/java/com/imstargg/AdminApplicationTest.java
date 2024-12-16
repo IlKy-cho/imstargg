d985748901b535c70f4d525fd981dc0d67e13d0e
Youngrae Cho <yrc9229@gmail.com>
2024-12-16 21:26:02 +0900

    test(admin): 어플리케이션 테스트 추가

---
+++ admin/src/test/java/com/imstargg/AdminApplicationTest.java
@@ -0,0 +1,14 @@
+package com.imstargg;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Tag("context")
+@SpringBootTest
+class AdminApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
