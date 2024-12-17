99c863fac4d7e03a46465dce27d92b716e1d8111
Youngrae Cho <yrc9229@gmail.com>
2024-12-17 12:15:18 +0900

    feat(aws-s3): 이미지 업로더

---
+++ support/aws-s3/src/main/java/com/imstargg/support/aws/s3/Image.java
@@ -0,0 +1,21 @@
+package com.imstargg.support.aws.s3;
+
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface Image {
+
+    String ext();
+
+    ImageContentType contentType() throws IOException;
+
+    long contentLength() throws IOException;
+
+    InputStream inputStream() throws IOException;
+
+    static Image resource(Resource resource) {
+        return new ResourceImage(resource);
+    }
+}
