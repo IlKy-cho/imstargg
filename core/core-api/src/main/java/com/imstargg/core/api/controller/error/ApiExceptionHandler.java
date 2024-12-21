144c3b04f31575dbac411294cba50a84ce680c02
Youngrae Cho <yrc9229@gmail.com>
2024-12-21 17:23:59 +0900

    feat(core): API 예외 핸들러 수정

--- core/core-api/src/main/java/com/imstargg/core/api/controller/error/ApiExceptionHandler.java
+++ core/core-api/src/main/java/com/imstargg/core/api/controller/error/ApiExceptionHandler.java
@@ -10,9 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
