213381ca304c3fa6f9a599f62c869f231407e6a9
Youngrae Cho <yrc9229@gmail.com>
2024-12-22 13:50:37 +0900

    feat(front): 헤더 및 네비게이션 수정

---
+++ imstargg-frontend/src/components/site-header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from 'next/image';
+import MainNav from "@/components/main-nav";
+
+export default function SiteHeader() {
+
+  return (
+    <header className="bg-zinc-900 text-white h-14 px-4 w-full z-50">
+      <div className="max-w-7xl mx-auto h-full flex items-center">
+        <div className="flex items-center gap-6">
+          <Link href="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity">
+            <Image src="/logo.png" alt="ImStarGG Logo" width={50} height={50}/>
+            <h1 className="text-xl font-bold">ImStarGG</h1>
+          </Link>
+          <MainNav />
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ imstargg-frontend/src/config/site.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+  { label: '맵', href: '/maps' },
+  { label: '브롤러', href: '/brawlers' },
+  { label: '경쟁전', href: '/competitive' },
+];
