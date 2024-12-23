8b414036f55dead8707f0c7d5ea2d5020f26dee0
Youngrae Cho <yrc9229@gmail.com>
2024-12-23 16:28:38 +0900

    feat(core): 이벤트, 도메인 프로퍼티 분리

--- core/core-domain/src/main/java/com/imstargg/core/domain/PlayerRenewEventPublisher.java
+++ core/core-domain/src/main/java/com/imstargg/core/domain/PlayerRenewEventPublisher.java
@@ -1,19 +1,30 @@
 package com.imstargg.core.domain;
 
 import com.imstargg.core.event.PlayerRenewalEvent;
+import com.imstargg.core.event.PlayerRenewalEventQueueProperties;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableConfigurationProperties(PlayerRenewalEventQueueProperties.class)
 public class PlayerRenewEventPublisher {
 
     private final SqsTemplate sqsTemplate;
+    private final PlayerRenewalEventQueueProperties queueProperties;
 
-    public PlayerRenewEventPublisher(SqsTemplate sqsTemplate) {
+    public PlayerRenewEventPublisher(
+            SqsTemplate sqsTemplate,
+            PlayerRenewalEventQueueProperties queueProperties
+    ) {
         this.sqsTemplate = sqsTemplate;
+        this.queueProperties = queueProperties;
     }
 
     public void publish(BrawlStarsTag tag) {
-        sqsTemplate.send(new PlayerRenewalEvent(tag.value()));
+        sqsTemplate.send(to -> to
+                .queue(queueProperties.name())
+                .payload(new PlayerRenewalEvent(tag.value()))
+        );
     }
 }
