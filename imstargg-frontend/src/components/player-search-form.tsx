8c2da331cdea25391c90eb6627a290f53a2132d2
Youngrae Cho <yrc9229@gmail.com>
2024-12-23 22:28:22 +0900

    feat(front): 불필요한 의존성 제거

--- imstargg-frontend/src/components/player-search-form.tsx
+++ imstargg-frontend/src/components/player-search-form.tsx
@@ -7,7 +7,7 @@ import {z} from "zod";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import React from "react";
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form";
 
 const formSchema = z.object({
   nameOrTag: z.string().min(0).max(100, {
