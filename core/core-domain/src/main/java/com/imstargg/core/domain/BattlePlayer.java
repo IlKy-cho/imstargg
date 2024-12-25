41ba81ea1eda7fea4ae7330097fb09ae3bc0c656
Youngrae Cho <yrc9229@gmail.com>
2024-12-25 16:47:22 +0900

    feat(core): 솔로랭크 티어 추가 및 응답 필드 include 설정

--- core/core-domain/src/main/java/com/imstargg/core/domain/BattlePlayer.java
+++ core/core-domain/src/main/java/com/imstargg/core/domain/BattlePlayer.java
@@ -1,8 +1,12 @@
 package com.imstargg.core.domain;
 
+import com.imstargg.core.enums.SoloRankTier;
+import jakarta.annotation.Nullable;
+
 public record BattlePlayer(
         BrawlStarsTag tag,
         String name,
+        @Nullable SoloRankTier soloRankTier,
         BattlePlayerBrawler brawler
 ) {
 }
