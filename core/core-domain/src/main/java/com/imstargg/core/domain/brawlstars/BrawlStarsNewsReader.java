cded14f823a9b08e54f240b975e5d7f1a32fb820
Youngrae Cho <yrc9229@gmail.com>
2024-12-25 21:51:15 +0900

    feat(core): 뉴스 캐시 제거

--- core/core-domain/src/main/java/com/imstargg/core/domain/brawlstars/BrawlStarsNewsReader.java
+++ core/core-domain/src/main/java/com/imstargg/core/domain/brawlstars/BrawlStarsNewsReader.java
@@ -1,13 +1,9 @@
 package com.imstargg.core.domain.brawlstars;
 
-import com.imstargg.core.config.CacheNames;
 import com.imstargg.core.domain.Slice;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
-@CacheConfig(cacheNames = CacheNames.BRAWL_STARS_NEWS)
 public class BrawlStarsNewsReader {
 
     private final BrawlStarsNewsRepository brawlStarsNewsRepository;
@@ -16,7 +12,6 @@ public class BrawlStarsNewsReader {
         this.brawlStarsNewsRepository = brawlStarsNewsRepository;
     }
 
-    @Cacheable(key = "'news:v1:' + #pageParam.language() + ':' + #pageParam.page()")
     public Slice<BrawlStarsNews> read(BrawlStarsNewsPageParam pageParam) {
         return brawlStarsNewsRepository.find(pageParam.language(), pageParam);
     }
