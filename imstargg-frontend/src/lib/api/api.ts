fcf0103e37501f3fe2ae94fd4896eaf134e5ffae
Youngrae Cho <yrc9229@gmail.com>
2024-12-25 16:27:51 +0900

    feat(front): api 캐시 옵션 추가, 브롤러 API 추가

--- imstargg-frontend/src/lib/api/api.ts
+++ imstargg-frontend/src/lib/api/api.ts
@@ -1,12 +1,21 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export async function fetchSearchPlayer(query: string): Promise<Response> {
+interface CacheOptions {
+  revalidate?: number | false;
+}
+
+export async function fetchSearchPlayer(query: string, options?: CacheOptions): Promise<Response> {
   const url = new URL(`${BASE_URL}/api/v1/player/search`);
   url.searchParams.append('query', query);
   console.log(`Fetch from ${url}`);
+  if (!options) {
+    return await fetch(url);
+  }
+
   return await fetch(url, {
     next: {
-      tags: ['player', 'search', query]
+      tags: ['player', 'search', query],
+      revalidate: options.revalidate,
     }
   });
 }
@@ -30,12 +39,48 @@ export async function fetchRenewPlayer(tag: string): Promise<Response> {
   });
 }
 
-export async function fetchGetPlayer(tag: string): Promise<Response> {
+export async function fetchGetPlayer(tag: string, options?: CacheOptions): Promise<Response> {
   const url = new URL(`${BASE_URL}/api/v1/players/${tag}`);
   console.log(`Fetch from ${url}`);
+  if (!options) {
+    return await fetch(url);
+  }
+
   return await fetch(url, {
     next: {
-      tags: ['players', tag]
+      tags: ['players', tag],
+      revalidate: options.revalidate
+    }
+  });
+}
+
+export async function fetchGetBattles(tag: string, page: number = 1, options?: CacheOptions): Promise<Response> {
+  const url = new URL(`${BASE_URL}/api/v1/players/${tag}/battles`);
+  url.searchParams.append('page', page.toString());
+  console.log(`Fetch from ${url}`);
+  if (!options) {
+    return await fetch(url);
+  }
+
+  return await fetch(url, {
+    next: {
+      tags: ['players', tag, 'battles', url.searchParams.toString()],
+      revalidate: options.revalidate
+    }
+  });
+}
+
+export async function fetchGetBrawlers(options?: CacheOptions): Promise<Response> {
+  const url = new URL(`${BASE_URL}/api/v1/brawlers`);
+  console.log(`Fetch from ${url}`);
+  if (!options) {
+    return await fetch(url);
+  }
+
+  return await fetch(url, {
+    next: {
+      tags: ['brawlers'],
+      revalidate: options.revalidate
     }
   });
 }
