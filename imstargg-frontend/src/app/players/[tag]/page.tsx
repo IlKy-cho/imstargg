576bb8d43e8a0c858f4035e7dc8560487f146660
Youngrae Cho <yrc9229@gmail.com>
2024-12-27 15:11:04 +0900

    fix(front): 태그 관련 오류 수정

--- imstargg-frontend/src/app/players/[tag]/page.tsx
+++ imstargg-frontend/src/app/players/[tag]/page.tsx
@@ -18,7 +18,7 @@ function PlayerNotFound({tag}: Readonly<PlayerNotFoundProps>) {
   return (
     <div className="space-y-10 my-10">
       <div className="text-center space-y-2">
-        <h1 className="text-2xl font-bold">태그 {decodeURIComponent(tag)} 플레이어를 찾을 수 없습니다</h1>
+        <h1 className="text-2xl font-bold">태그 {tag} 플레이어를 찾을 수 없습니다</h1>
         <p className="text-gray-500">다른 플레이어를 검색해보세요</p>
       </div>
 
@@ -37,22 +37,23 @@ type Props = {
 
 export default async function PlayerPage({params}: Readonly<Props>) {
   const { tag } = await params;
+  const decodedTag = decodeURIComponent(tag);
   const playerResponse = await getPlayer(tag);
 
   if (!playerResponse.player) {
-    return <PlayerNotFound tag={tag}/>;
+    return <PlayerNotFound tag={decodedTag}/>;
   }
   const player = playerResponse.player;
   const brawlers = await getBrawlers();
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-2">
       <div className="flex flex-col lg:flex-row gap-4 bg-cover bg-center bg-no-repeat bg-brawl-stars-lobby p-4">
         <div className="flex-1">
           <PlayerProfile player={player}/>
         </div>
       </div>
-      <PlayerBattleList tag={tag} brawlerList={brawlers}/>
+      <PlayerBattleList tag={decodedTag} brawlerList={brawlers}/>
     </div>
   );
 }
