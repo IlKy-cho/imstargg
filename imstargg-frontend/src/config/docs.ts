ccfb3c6ca8fee52aaadf62873a1a17b358747f0d
Youngrae Cho <yrc9229@gmail.com>
2024-12-27 09:15:40 +0900

    feat(front): 브롤러 링크

--- imstargg-frontend/src/config/docs.ts
+++ imstargg-frontend/src/config/docs.ts
@@ -1,5 +1,9 @@
+import {Brawler} from "@/model/Brawler";
+
 export const navItems = [
   { label: '맵', href: '/maps' },
   { label: '브롤러', href: '/brawlers' },
   { label: '경쟁전', href: '/competitive' },
 ];
+
+export const brawlerHref = (brawler: Brawler) => `/brawlers/${brawler.id}`;
