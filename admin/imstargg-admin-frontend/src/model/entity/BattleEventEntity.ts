a1619191a7ddfdc87db531885c02d7afeba2b61f
Youngrae Cho <yrc9229@gmail.com>
2024-12-29 21:04:58 +0900

    feat(admin): 모델 수정

--- admin/imstargg-admin-frontend/src/model/entity/BattleEventEntity.ts
+++ admin/imstargg-admin-frontend/src/model/entity/BattleEventEntity.ts
@@ -1,9 +1,9 @@
 import BaseEntity from "@/model/entity/BaseEntity";
-import {BattleEventModeType} from "@/model/enums/BattleEventMode";
+import {BattleEventMode} from "@/model/enums/BattleEventMode";
 
 export default interface BattleEventEntity extends BaseEntity {
   id: number;
   brawlStarsId: number;
-  mode: BattleEventModeType;
+  mode: BattleEventMode;
   mapId: number;
 }
--- admin/imstargg-admin-frontend/src/model/entity/GearEntity.ts
+++ admin/imstargg-admin-frontend/src/model/entity/GearEntity.ts
@@ -1,9 +1,9 @@
 import BaseEntity from "@/model/entity/BaseEntity";
-import {GearRarityType} from "@/model/enums/GearRarity";
+import {GearRarity} from "@/model/enums/GearRarity";
 
 export default interface GearEntity extends BaseEntity {
   id: number;
   brawlStarsId: string;
   nameMessageCode: string;
-  rarity: GearRarityType;
+  rarity: GearRarity;
 }
