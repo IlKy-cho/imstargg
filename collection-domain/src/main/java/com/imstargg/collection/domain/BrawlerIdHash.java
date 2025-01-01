98ff3018207235ef49aefbe5130e4f678baff4a2
Youngrae Cho <yrc9229@gmail.com>
2025-01-01 20:48:18 +0900

    feat(collection): 브롤러 아이디 해시 생성 로직 이전

--- collection-domain/src/main/java/com/imstargg/collection/domain/BrawlerIdHash.java
+++ collection-domain/src/main/java/com/imstargg/collection/domain/BrawlerIdHash.java
@@ -1,13 +1,42 @@
 package com.imstargg.collection.domain;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public record BrawlerIdHash(
-        List<Long> brawlerIds,
         byte[] value
 ) {
 
+    public static BrawlerIdHash of(List<Long> brawlerIds) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES * brawlerIds.size());
+        brawlerIds.stream()
+                .sorted()
+                .forEach(buffer::putLong);
+        return new BrawlerIdHash(buffer.array());
+    }
+
     public int num() {
-        return brawlerIds.size();
+        return value.length / Long.BYTES;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BrawlerIdHash that = (BrawlerIdHash) o;
+        return Objects.deepEquals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return "BrawlerIdHash{" +
+                "value=" + Arrays.toString(value) +
+                '}';
     }
 }
