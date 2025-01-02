abe7312b356716911b1a6748138bc43fcd9b1e20
Youngrae Cho <yrc9229@gmail.com>
2025-01-02 12:32:39 +0900

    fix(db-collection): 아이디 생성전략 누락

--- storage/db-collection/src/main/java/com/imstargg/storage/db/core/statistics/StatisticsCollectedBattleRankCollectionEntity.java
+++ storage/db-collection/src/main/java/com/imstargg/storage/db/core/statistics/StatisticsCollectedBattleRankCollectionEntity.java
@@ -3,6 +3,8 @@ package com.imstargg.storage.db.core.statistics;
 import com.imstargg.storage.db.core.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -17,6 +19,7 @@ import jakarta.persistence.UniqueConstraint;
 public class StatisticsCollectedBattleRankCollectionEntity extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "stats_collected_battle_rank_id")
     private Long id;
 
