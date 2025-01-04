ee7b59cc4d15acf84f28bba1c3578be2c81af3bb
Youngrae Cho <yrc9229@gmail.com>
2025-01-04 11:57:34 +0900

    feat(db-core): 통계 레파지토리

---
+++ storage/db-core/src/main/java/com/imstargg/storage/db/core/statistics/BrawlersBattleRankStatisticsJpaRepository.java
@@ -0,0 +1,6 @@
+package com.imstargg.storage.db.core.statistics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrawlersBattleRankStatisticsJpaRepository extends JpaRepository<BrawlersBattleRankStatisticsEntity, Long> {
+}
