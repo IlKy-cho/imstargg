26781515c78956827cce7d3065a9dd9d980bcb15
Youngrae Cho <yrc9229@gmail.com>
2025-01-07 16:51:59 +0900

    feat(front): 네비게이션 라벨 수정

--- imstargg-frontend/src/lib/api/brawler.ts
+++ imstargg-frontend/src/lib/api/brawler.ts
@@ -3,8 +3,7 @@ import {ApiError, fetchGetBrawlers} from "@/lib/api/api";
 import {ListResponse} from "@/model/response/ListResponse";
 
 export async function getBrawlers() : Promise<Brawler[]> {
-  // const response = await fetchGetBrawlers({revalidate: 60 * 60});
-  const response = await fetchGetBrawlers();
+  const response = await fetchGetBrawlers({revalidate: 60 * 60});
 
   if (response.ok) {
     const data = await response.json() as ListResponse<Brawler>;
