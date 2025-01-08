050a6a08ee0754148daf02d58a43dede79f1c403
Youngrae Cho <yrc9229@gmail.com>
2025-01-08 17:39:35 +0900

    feat(core): 이벤트 단건 조회 API

--- core/core-domain/src/main/java/com/imstargg/core/error/CoreErrorType.java
+++ core/core-domain/src/main/java/com/imstargg/core/error/CoreErrorType.java
@@ -12,7 +12,9 @@ public enum CoreErrorType {
     PLAYER_NOT_FOUND(CoreErrorKind.NOT_FOUND, "플레이어를 찾을 수 없습니다.", INFO),
     PLAYER_ALREADY_RENEWED(CoreErrorKind.FORBIDDEN, "이미 갱신된 플레이어입니다.", INFO),
     PLAYER_RENEW_UNAVAILABLE(CoreErrorKind.UNAVAILABLE,
-            "플레이어 갱신 요청이 많아 처리가 불가능합니다. 잠시후에 다시 시도해주세요.", WARN)
+            "플레이어 갱신 요청이 많아 처리가 불가능합니다. 잠시후에 다시 시도해주세요.", WARN),
+
+    BATTLE_EVENT_NOT_FOUND(CoreErrorKind.NOT_FOUND, "존재하지 않는 이벤트 입니다.", INFO),
     ;
 
     private final CoreErrorKind kind;
--- storage/db-core/src/main/java/com/imstargg/storage/db/core/brawlstars/BattleEventJpaRepository.java
+++ storage/db-core/src/main/java/com/imstargg/storage/db/core/brawlstars/BattleEventJpaRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface BattleEventJpaRepository extends JpaRepository<BattleEventEntity, Long> {
+
+    Optional<BattleEventEntity> findByBrawlStarsId(Long brawlStarsId);
 
     List<BattleEventEntity> findAllByBrawlStarsIdIn(Collection<Long> brawlStarsIds);
 }
