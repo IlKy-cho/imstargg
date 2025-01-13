1af070db70f00cd0d34d454b7e4520067c507dc2
Youngrae Cho <yrc9229@gmail.com>
2025-01-13 12:13:17 +0900

    feat(front): 갱신 로직 수정

--- imstargg-frontend/src/components/player-renew-button.tsx
+++ imstargg-frontend/src/components/player-renew-button.tsx
@@ -4,9 +4,9 @@ import {LoadingButton} from "@/components/ui/expansion/loading-button";
 import {useState, useEffect, useCallback} from "react";
 import {Player} from "@/model/Player";
 import {getPlayerRenewalStatus, renewPlayer} from "@/lib/api/player";
-import {ApiErrorTypeValue} from "@/model/response/ApiResponse";
 import {toast} from "sonner";
 import {useInterval} from "usehooks-ts";
+import { ApiError, ApiErrorTypeValue } from "@/model/response/error";
 
 export default function PlayerRenewButton({player}: Readonly<{ player: Player }>) {
   const [renewalEnabled, setRenewalEnabled] = useState(false);
@@ -33,31 +33,35 @@ export default function PlayerRenewButton({player}: Readonly<{ player: Player }>
   }, [player.updatedAt, calculateTimeStatus]);
 
   const handleRenew = async () => {
-
     setLoading(true);
     try {
-      const response = await renewPlayer(player.tag);
-      if (response.ok) {
-        setRenewalEnabled(false);
-        
-        const checkRenewalStatus = async () => {
-          const status = await getPlayerRenewalStatus(player.tag);
-          if (!status.renewing) {
-            window.location.reload();
-          } else {
-            setTimeout(checkRenewalStatus, 1000);
-          }
-        };
+      await renewPlayer(player.tag);
+      setRenewalEnabled(false);
 
-        await checkRenewalStatus();
+      const checkRenewalStatus = async () => {
+        const status = await getPlayerRenewalStatus(player.tag);
+        if (!status.renewing) {
+          window.location.reload();
+        } else {
+          setTimeout(checkRenewalStatus, 1000);
+        }
+      };
 
-      } else if (response.error?.type === ApiErrorTypeValue.PLAYER_ALREADY_RENEWED) {
-        toast("아직 새로고침이 불가능합니다. 잠시 후 다시 시도해주세요.");
-      } else if (response.error?.type === ApiErrorTypeValue.PLAYER_RENEW_UNAVAILABLE) {
-        toast("현재 새로고침 요청이 많아서 처리할 수 없습니다. 잠시 후 다시 시도해주세요.");
-      }
+      await checkRenewalStatus();
     } catch (error) {
-      console.error('Failed to renew player:', error);
+      if (error instanceof ApiError) {
+        if (error.error?.type === ApiErrorTypeValue.PLAYER_ALREADY_RENEWED) {
+          toast("아직 새로고침이 불가능합니다. 잠시 후 다시 시도해주세요.");
+        } else if (error.error?.type === ApiErrorTypeValue.PLAYER_RENEW_UNAVAILABLE) {
+          toast("현재 새로고침 요청이 많아서 처리할 수 없습니다. 잠시 후 다시 시도해주세요.");
+        } else {
+          console.error('Failed to renew player:', error);
+          toast("플레이어 정보 새로고침 중 오류가 발생했습니다.");
+        }
+      } else {
+        console.error('Unexpected error:', error);
+        toast("예기치 않은 오류가 발생했습니다.");
+      }
     } finally {
       setLoading(false);
     }
