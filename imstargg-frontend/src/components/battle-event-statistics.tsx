075f0be7f4d4410032481e6e3932ffb3567ff4c5
Youngrae Cho <yrc9229@gmail.com>
2025-01-15 12:15:05 +0900

    feat(front): 브롤러 통계 컴포넌트

--- imstargg-frontend/src/components/battle-event-statistics.tsx
+++ imstargg-frontend/src/components/battle-event-statistics.tsx
@@ -1,11 +1,220 @@
-type Props = {
+"use client";
 
+import {
+  BattleEventBrawlerRankStatistics as IBattleEventBrawlerRankStatistics
+} from "@/model/BattleEventBrawlerRankStatistics";
+import {
+  BattleEventBrawlersRankStatistics as IBattleEventBrawlersRankStatistics
+} from "@/model/BattleEventBrawlersRankStatistics";
+import {Brawler, Brawlers} from "@/model/Brawler";
+import {ColumnDef} from "@tanstack/react-table";
+import {DataTableColumnHeader} from "@/components/ui/datatable/column-header";
+import BrawlerProfileImage from "@/components/brawler-profile-image";
+import {DataTable} from "@/components/ui/datatable/data-table";
+import {
+  BattleEventBrawlerResultStatistics as IBattleEventBrawlerResultStatistics
+} from "@/model/BattleEventBrawlerResultStatistics";
+import {
+  BattleEventBrawlersResultStatistics as IBattleEventBrawlersResultStatistics
+} from "@/model/BattleEventBrawlersResultStatistics";
+
+function toPercentage(value: number): string {
+  return `${(value * 100).toFixed(2)}%`;
 }
 
-export default async function BattleEventStatistics() {
+function BrawlerCell({brawler} : {brawler: Brawler | null}) {
   return (
-    <div>
-      <h1>BattleEventStatistics</h1>
+    <div className="flex flex-col gap-1">
+      <BrawlerProfileImage
+        brawler={brawler}
+        size="sm"
+      />
+      <span className="text-xs">{brawler ? brawler.name : "❓"}</span>
     </div>
-  );
+  )
 }
+
+function BrawlersCell({brawlers} : {brawlers: Array<Brawler | null>}) {
+  return (
+    <div className="flex gap-1">
+      {brawlers.map(brawler => (
+        <BrawlerProfileImage
+          key={brawler?.id}
+          brawler={brawler}
+          size="xs"
+        />
+      ))}
+    </div>
+  )
+}
+
+type BrawlerRankStatisticsProps = {
+  brawlerList: Brawler[],
+  brawlerRankStatsList: IBattleEventBrawlerRankStatistics[]
+};
+
+export function BattleEventBrawlerRankStatistics(
+  {brawlerList, brawlerRankStatsList}: Readonly<BrawlerRankStatisticsProps>
+) {
+  const brawlers = new Brawlers(brawlerList);
+  const columns: ColumnDef<IBattleEventBrawlerRankStatistics>[] = [
+    {
+      accessorKey: "brawlerId",
+      enableSorting: false,
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"브롤러"}/>,
+      cell: ({row}) => {
+        const brawler = brawlers.find(row.original.brawlerId);
+        return (
+          <BrawlerCell brawler={brawler}/>
+        );
+      },
+    },
+    {
+      accessorKey: "averageRank",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"평균 랭크"}/>,
+      cell: ({row}) => row.original.averageRank.toFixed(2),
+    },
+    {
+      accessorKey: "pickRate",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"픽률"}/>,
+      cell: ({row}) => toPercentage(row.original.pickRate),
+    }
+  ];
+
+  return (
+    <DataTable columns={columns} data={brawlerRankStatsList}/>
+  )
+}
+
+type BrawlersRankStatisticsProps = {
+  brawlerList: Brawler[],
+  brawlersRankStatsList: IBattleEventBrawlersRankStatistics[]
+};
+
+export function BattleEventBrawlersRankStatistics(
+  {brawlerList, brawlersRankStatsList}: Readonly<BrawlersRankStatisticsProps>
+) {
+  const brawlers = new Brawlers(brawlerList);
+  const columns: ColumnDef<IBattleEventBrawlersRankStatistics>[] = [
+    {
+      accessorKey: "brawlerIds",
+      enableSorting: false,
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"브롤러 조합"}/>,
+      cell: ({row}) => {
+        const brawlersList = row.original.brawlerIds.map(id => brawlers.find(id));
+        return (
+          <BrawlersCell brawlers={brawlersList}/>
+        );
+      },
+    },
+    {
+      accessorKey: "averageRank",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"평균 랭크"}/>,
+      cell: ({row}) => row.original.averageRank.toFixed(2),
+    },
+    {
+      accessorKey: "pickRate",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"픽률"}/>,
+      cell: ({row}) => toPercentage(row.original.pickRate),
+    }
+  ];
+
+  return (
+    <DataTable columns={columns} data={brawlersRankStatsList} paginated={true}/>
+  )
+}
+
+type BrawlerResultStatisticsProps = {
+  brawlerList: Brawler[],
+  brawlerResultStatsList: IBattleEventBrawlerResultStatistics[]
+};
+
+export function BattleEventBrawlerResultStatistics(
+  {brawlerList, brawlerResultStatsList}: Readonly<BrawlerResultStatisticsProps>
+) {
+  const brawlers = new Brawlers(brawlerList);
+  const columns: ColumnDef<IBattleEventBrawlerResultStatistics>[] = [
+    {
+      accessorKey: "brawlerId",
+      enableSorting: false,
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"브롤러"}/>,
+      cell: ({row}) => {
+        const brawler = brawlers.find(row.original.brawlerId);
+        return (
+          <BrawlerCell brawler={brawler}/>
+        );
+      },
+    },
+    {
+      accessorKey: "winRate",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"승률"}/>,
+      cell: ({row}) => toPercentage(row.original.winRate),
+    },
+    {
+      accessorKey: "starPlayerRate",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"스타플레이어"}/>,
+      cell: ({row}) => toPercentage(row.original.starPlayerRate),
+    },
+    {
+      accessorKey: "pickRate",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"픽률"}/>,
+      cell: ({row}) => toPercentage(row.original.pickRate),
+    }
+  ];
+
+  return (
+    <DataTable columns={columns} data={brawlerResultStatsList}/>
+  )
+}
+
+type BrawlersResultStatisticsProps = {
+  brawlerList: Brawler[],
+  brawlersResultStatsList: IBattleEventBrawlersResultStatistics[]
+};
+
+export function BattleEventBrawlersResultStatistics(
+  {brawlerList, brawlersResultStatsList}: Readonly<BrawlersResultStatisticsProps>
+) {
+  const brawlers = new Brawlers(brawlerList);
+  const columns: ColumnDef<IBattleEventBrawlersResultStatistics>[] = [
+    {
+      accessorKey: "brawlerIds",
+      enableSorting: false,
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"브롤러 조합"}/>,
+      cell: ({row}) => {
+        const brawlersList = row.original.brawlerIds.map(id => brawlers.find(id));
+        return (
+          <BrawlersCell brawlers={brawlersList}/>
+        );
+      },
+    },
+    {
+      accessorKey: "winRate",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"승률"}/>,
+      cell: ({row}) => toPercentage(row.original.winRate),
+    },
+    {
+      accessorKey: "pickRate",
+      header: ({column}) =>
+        <DataTableColumnHeader column={column} title={"픽률"}/>,
+      cell: ({row}) => toPercentage(row.original.pickRate),
+    }
+  ];
+
+  return (
+    <DataTable columns={columns} data={brawlersResultStatsList} paginated={true}/>
+  )
+}
+
