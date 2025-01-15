0737ed1aa5b02b0bc6e8d65e5df1959c20f8785a
Youngrae Cho <yrc9229@gmail.com>
2025-01-15 13:16:26 +0900

    feat(front): layout 수정

--- imstargg-frontend/src/components/side-area.tsx
+++ imstargg-frontend/src/components/side-area.tsx
@@ -1,9 +1,6 @@
 export default function SideArea() {
   return (
-    <div className="hidden lg:block w-[200px]">
-      <div className="sticky top-4 p-2">
-        {/* 측면 영역 */}
-      </div>
+    <div className="max-w-[300px]">
     </div>
   );
 }
