ff28b348a0bf8119c7b03cb7e09384ed7dfe86df
Youngrae Cho <yrc9229@gmail.com>
2025-01-20 20:00:38 +0900

    feat(core): 1주일 스트림 로직 분리

--- core/core-domain/src/main/java/com/imstargg/core/domain/statistics/BattleEventBrawlersResultStatisticsParams.java
+++ core/core-domain/src/main/java/com/imstargg/core/domain/statistics/BattleEventBrawlersResultStatisticsParams.java
@@ -1,6 +1,7 @@
 package com.imstargg.core.domain.statistics;
 
 import com.imstargg.core.domain.BrawlStarsId;
+import com.imstargg.core.domain.utils.DateUtils;
 import com.imstargg.core.enums.SoloRankTierRange;
 import com.imstargg.core.enums.SoloRankTierRangeRange;
 import com.imstargg.core.enums.TrophyRange;
@@ -8,6 +9,7 @@ import com.imstargg.core.enums.TrophyRangeRange;
 import jakarta.annotation.Nullable;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -22,13 +24,13 @@ public record BattleEventBrawlersResultStatisticsParams(
 
     public List<BattleEventBrawlersResultStatisticsParam> toParamList() {
         if (trophyRangeRange != null) {
-            return battleDateWeekStream().flatMap(battleDate ->
+            return DateUtils.lastAWeekStream(date).flatMap(battleDate ->
                     trophyRangeRange.getRanges().stream().flatMap(trophyRange ->
                             mapParam(battleDate, trophyRange, null, duplicateBrawler)
                     )
             ).toList();
         } else if (soloRankTierRangeRange != null) {
-            return battleDateWeekStream().flatMap(battleDate ->
+            return DateUtils.lastAWeekStream(date).flatMap(battleDate ->
                     soloRankTierRangeRange.getRanges().stream().flatMap(soloRankTierRange ->
                             mapParam(battleDate, null, soloRankTierRange, duplicateBrawler)
                     )
@@ -38,45 +40,31 @@ public record BattleEventBrawlersResultStatisticsParams(
         return List.of();
     }
 
-    private Stream<LocalDate> battleDateWeekStream() {
-        return Stream.iterate(date, battleDate -> battleDate.minusDays(1))
-                .limit(7);
-    }
-
     private Stream<BattleEventBrawlersResultStatisticsParam> mapParam(
             LocalDate battleDate,
             @Nullable TrophyRange trophyRange,
             @Nullable SoloRankTierRange soloRankTierRange,
             boolean duplicateBrawler
     ) {
-        return !duplicateBrawler
-                ? Stream.of(
-                new BattleEventBrawlersResultStatisticsParam(
-                        eventId,
-                        battleDate,
-                        trophyRange,
-                        soloRankTierRange,
-                        brawlersNum,
-                        false
-                ))
-                :
-                Stream.of(
-                        new BattleEventBrawlersResultStatisticsParam(
-                                eventId,
-                                battleDate,
-                                trophyRange,
-                                soloRankTierRange,
-                                brawlersNum,
-                                false
-                        ),
-                        new BattleEventBrawlersResultStatisticsParam(
-                                eventId,
-                                battleDate,
-                                trophyRange,
-                                soloRankTierRange,
-                                brawlersNum,
-                                true
-                        )
-                );
+        List<BattleEventBrawlersResultStatisticsParam> result = new ArrayList<>();
+        result.add(new BattleEventBrawlersResultStatisticsParam(
+                eventId,
+                battleDate,
+                trophyRange,
+                soloRankTierRange,
+                brawlersNum,
+                false
+        ));
+        if (duplicateBrawler) {
+            result.add(new BattleEventBrawlersResultStatisticsParam(
+                    eventId,
+                    battleDate,
+                    trophyRange,
+                    soloRankTierRange,
+                    brawlersNum,
+                    true
+            ));
+        }
+        return result.stream();
     }
 }
