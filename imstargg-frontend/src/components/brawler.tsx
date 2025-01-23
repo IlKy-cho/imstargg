09bfb13d9305e920a9441e48e4b74e65dd16101a
Youngrae Cho <yrc9229@gmail.com>
2025-01-23 13:11:15 +0900

    feat(front): 플레이어 페이지 메타데이터 수정, 파워레벨 컴포넌트 분리

--- imstargg-frontend/src/components/brawler.tsx
+++ imstargg-frontend/src/components/brawler.tsx
@@ -21,3 +21,20 @@ export const brawlerBackgroundColor = (brawler: Brawler) => {
       return 'bg-[#FDF255]';
   }
 }
+
+export function PowerLevel({value}: { value: number }) {
+  return (
+    <div className="relative inline-flex items-center justify-center">
+      <div
+        className="w-6 h-6 rounded-full bg-gradient-to-br from-[#E84BCE] to-[#CF30B5] flex items-center justify-center shadow-lg border-[0.5px] border-black">
+        <div className="w-4 h-4 rounded-full bg-[#6c1473] flex items-center justify-center border-[0.5px] border-black">
+          <span className="text-white font-bold text-xs" style={{
+            textShadow: '-0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000'
+          }}>
+            {value}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
