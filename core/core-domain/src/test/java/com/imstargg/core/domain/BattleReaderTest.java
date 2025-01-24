f40e581b0cfc4103f2f3dba6413aff78d77e69b3
Youngrae Cho <yrc9229@gmail.com>
2025-01-24 22:03:12 +0900

    test(core): BattleReader

---
+++ core/core-domain/src/test/java/com/imstargg/core/domain/BattleReaderTest.java
@@ -0,0 +1,42 @@
+package com.imstargg.core.domain;
+
+import com.imstargg.core.enums.Language;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class BattleReaderTest {
+
+    @Mock
+    private BattleRepository battleRepository;
+
+    @InjectMocks
+    private BattleReader battleReader;
+
+    @Test
+    void 플레이어의_전적을_조회한다() {
+        // given
+        Player player = new PlayerFixture().build();
+        int page = 1;
+        Language language = Language.KOREAN;
+        List<PlayerBattle> playerBattles = List.of(new PlayerBattleFixture().build());
+        given(battleRepository.find(player, page, language)).willReturn(new Slice<>(
+                playerBattles, false
+        ));
+
+        // when
+        var result = battleReader.getList(player, page, language);
+
+        // then
+        assertThat(result.content()).isEqualTo(playerBattles);
+        assertThat(result.hasNext()).isFalse();
+    }
+}
