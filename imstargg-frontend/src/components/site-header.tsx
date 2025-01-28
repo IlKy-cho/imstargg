1e408670d1060b9a3507af9617423e8e66164852
Youngrae Cho <yrc9229@gmail.com>
2025-01-28 21:43:54 +0900

    feat(front): 헤더 수정

--- imstargg-frontend/src/components/site-header.tsx
+++ imstargg-frontend/src/components/site-header.tsx
@@ -6,11 +6,11 @@ import {meta} from "@/config/site";
 export default function SiteHeader() {
 
   return (
-    <header className="bg-zinc-900 text-white h-14 px-4 w-full z-50">
+    <header className="bg-zinc-900 text-white h-12 w-full z-50">
       <div className="max-w-7xl mx-auto h-full flex items-center">
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity">
-            <Image src={meta.logo} alt="ImStarGG Logo" width={50} height={50}/>
+            <Image src={meta.logo} alt="ImStarGG Logo" width={40} height={40}/>
             <h1 className="text-xl font-bold">ImStarGG</h1>
           </Link>
           <MainNav />
