874a107ad94b5dca3a0179445bde676c3954942c
Youngrae Cho <yrc9229@gmail.com>
2025-01-31 19:08:05 +0900

    feat(front): 빌드 에러 수정

--- imstargg-frontend/src/app/player/search/page.tsx
+++ imstargg-frontend/src/app/player/search/page.tsx
@@ -45,7 +45,7 @@ export default function PlayerSearchResultPage() {
     };
 
     fetchPlayers();
-  }, [query, router]);
+  }, [query, router, addSearchTerm]);
 
   return (
     <div className="w-full max-w-xl mx-auto p-1">
