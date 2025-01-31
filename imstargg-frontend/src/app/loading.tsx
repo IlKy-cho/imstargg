2f738d7fcaedaf4f4ef4344e04b70a26ba5933a8
Youngrae Cho <yrc9229@gmail.com>
2025-01-31 14:20:59 +0900

    feat(front): 로딩 크기 수정

--- imstargg-frontend/src/app/loading.tsx
+++ imstargg-frontend/src/app/loading.tsx
@@ -3,7 +3,7 @@ import { LoaderIcon } from "lucide-react";
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <LoaderIcon className="animate-spin w-12 h-12" />
+      <LoaderIcon className="animate-spin w-8 h-8 sm:w-12 sm:h-12" />
     </div>
   );
 } 
