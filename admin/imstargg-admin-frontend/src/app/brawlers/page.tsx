90709d39ece8d1281659eebc7720a4087f8f521c
Youngrae Cho <yrc9229@gmail.com>
2025-02-05 16:21:14 +0900

    feat(admin): 프론트 브롤러 리스트 API 호출부 분리

--- admin/imstargg-admin-frontend/src/app/brawlers/page.tsx
+++ admin/imstargg-admin-frontend/src/app/brawlers/page.tsx
@@ -1,18 +1,18 @@
-import { BrawlerList } from "./_components/brawler-list";
-import { BrawlerAdd } from "./_components/brawler-add";
-import { GearList } from "./_components/gear-list";
+import {BrawlerList} from "./_components/brawler-list";
+import {BrawlerAdd} from "./_components/brawler-add";
+import {getBrawlers} from "@/lib/api/brawler";
 
 export default async function BrawlersPage() {
+  const brawlers = await getBrawlers();
   return (
     <>
       <div className="flex justify-between items-center">
         <h1>브롤러 목록</h1>
         <div className="space-x-2">
-          <GearList />
           <BrawlerAdd />
         </div>
       </div>
-      <BrawlerList />
+      <BrawlerList brawlers={brawlers}/>
     </>
   );
 }
