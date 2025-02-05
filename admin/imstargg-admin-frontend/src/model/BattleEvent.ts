2eb3bc4818698205f9e69302eb5daf049b4b10fd
Youngrae Cho <yrc9229@gmail.com>
2025-02-05 21:58:08 +0900

    feat(admin): 이벤트에 배틀 모드 추가

--- admin/imstargg-admin-frontend/src/model/BattleEvent.ts
+++ admin/imstargg-admin-frontend/src/model/BattleEvent.ts
@@ -4,5 +4,6 @@ import {BattleEntityEvent} from "@/model/entity/BattleEntityEvent";
 export interface BattleEvent {
   entity: BattleEntityEvent;
   map: BattleEventMap;
+  battleMode: string;
   latestBattleTime: Date | null;
 }
