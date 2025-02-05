d49c0cc1ad6258a0f694f983fd9ff67089398f34
Youngrae Cho <yrc9229@gmail.com>
2025-02-05 21:09:03 +0900

    feat(admin): 초기 메시지 값 분리

--- admin/imstargg-admin-frontend/src/lib/message.ts
+++ admin/imstargg-admin-frontend/src/lib/message.ts
@@ -1,8 +1,11 @@
 import {MessageEntity} from "@/model/entity/MessageEntity";
-import { languageOrder } from "@/model/enums/Language";
+import {Language, languageOrder, LanguageValues} from "@/model/enums/Language";
 
 export function messagesContent(messages: MessageEntity[]) {
   return messages.sort((a, b) => languageOrder(a.lang) - languageOrder(b.lang))
     .map((message) => `${message.content}(${message.lang})`)
     .join(" | ");
 }
+
+export const initialNewMessages: Record<Language, string> =
+  Object.fromEntries(LanguageValues.map(lang => [lang, ""])) as Record<Language, string>;
