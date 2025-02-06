c21c133f8e40cc11da062b1e16bfb3ecb16a8357
Youngrae Cho <yrc9229@gmail.com>
2025-02-06 10:46:04 +0900

    feat(admin): 기어 리스트 페이지 이미지 적용

--- admin/imstargg-admin-frontend/src/app/gears/_components/gear-list.tsx
+++ admin/imstargg-admin-frontend/src/app/gears/_components/gear-list.tsx
@@ -10,13 +10,15 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { messagesContent } from "@/lib/message";
-
+import { imageUrl } from "@/lib/image";
+import Image from "next/image";
 export async function GearList({gears}: Readonly<{gears: Gear[]}>) {
   return (
     <Table>
       <TableHeader className="bg-white">
         <TableRow>
           <TableHead>ID</TableHead>
+          <TableHead className="w-64">이미지</TableHead>
           <TableHead>희귀도</TableHead>
           <TableHead>이름</TableHead>
           <TableHead className="text-right">메뉴</TableHead>
@@ -26,10 +28,22 @@ export async function GearList({gears}: Readonly<{gears: Gear[]}>) {
         {gears.map((gear) => (
           <TableRow key={gear.entity.brawlStarsId}>
             <TableCell>{gear.entity.brawlStarsId}</TableCell>
-            <TableCell>{gear.entity.rarity}</TableCell>
             <TableCell>
-              {messagesContent(gear.names)}
+              {gear.image ? (
+                <Image
+                  src={imageUrl(gear.image)}
+                  alt={gear.entity.id + " 이미지"}
+                  width={64}
+                  height={64}
+                />
+              ) : (
+                <div className="flex items-center justify-center w-16 h-16 bg-gray-100">
+                  <span className="text-xl text-gray-400">X</span>
+                </div>
+              )}
             </TableCell>
+            <TableCell>{gear.entity.rarity}</TableCell>
+            <TableCell>{messagesContent(gear.names)}</TableCell>
             <TableCell className="text-right">
 
             </TableCell>
