133742a5ac911ad572645084f7a4159c8d32c4f9
Youngrae Cho <yrc9229@gmail.com>
2025-02-08 17:37:08 +0900

    feat(db-core): rds cache 추가

---
+++ storage/db-core/src/main/java/com/imstargg/storage/db/core/cache/RdsCacheJpaRepository.java
@@ -0,0 +1,6 @@
+package com.imstargg.storage.db.core.cache;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RdsCacheJpaRepository extends JpaRepository<RdsCacheEntity, Long> {
+}
