7975ce329ff15d5551eb2afe448a358a92652fce
Youngrae Cho <yrc9229@gmail.com>
2025-02-09 01:33:14 +0900

    feat(core): 브롤러 소유율 API

---
+++ core/core-api/src/main/java/com/imstargg/core/api/controller/v1/response/ItemRateResponse.java
@@ -0,0 +1,13 @@
+package com.imstargg.core.api.controller.v1.response;
+
+import com.imstargg.core.domain.statistics.ItemRate;
+
+public record ItemRateResponse(
+        long id,
+        double value
+) {
+
+    public static ItemRateResponse of(ItemRate itemRate) {
+        return new ItemRateResponse(itemRate.id().value(), itemRate.value());
+    }
+}
