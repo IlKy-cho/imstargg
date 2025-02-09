267ce0ac9902aa9bf017409d49031e88c5fdd715
Youngrae Cho <yrc9229@gmail.com>
2025-02-09 22:22:51 +0900

    feat(db-core): rds 캐시 제거, 브롤러 아이템 카운트 엔티티 추가

---
+++ storage/db-core/src/main/java/com/imstargg/storage/db/core/statistics/BrawlerItemCountEntity.java
@@ -0,0 +1,54 @@
+package com.imstargg.storage.db.core.statistics;
+
+import com.imstargg.storage.db.core.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+        name = "brawler_item_count",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_brawler_item_count", columnNames = {"brawler_brawlstars_id", "item_brawlstars_id"}
+                )
+        }
+)
+public class BrawlerItemCountEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brawler_brawlstars_id", updatable = false, nullable = false)
+    private long brawlerBrawlStarsId;
+
+    @Column(name = "item_brawlstars_id", updatable = false, nullable = false)
+    private long itemBrawlStarsId;
+
+    @Column(name = "item_count", nullable = false)
+    private int count;
+
+    protected BrawlerItemCountEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public long getBrawlerBrawlStarsId() {
+        return brawlerBrawlStarsId;
+    }
+
+    public long getItemBrawlStarsId() {
+        return itemBrawlStarsId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ storage/db-core/src/main/java/com/imstargg/storage/db/core/statistics/BrawlerItemCountJpaRepository.java
@@ -0,0 +1,6 @@
+package com.imstargg.storage.db.core.statistics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrawlerItemCountJpaRepository extends JpaRepository<BrawlerItemCountEntity, Long> {
+}
