96275c0d4f057f22361d548525fe4e91fac02bfe
Youngrae Cho <yrc9229@gmail.com>
2025-02-09 09:25:35 +0900

    feat(front): 브롤러 프로필 크기 설정

--- imstargg-frontend/src/components/brawler-profile.tsx
+++ imstargg-frontend/src/components/brawler-profile.tsx
@@ -14,11 +14,11 @@ type BrawlerProfileProps = { brawler: Brawler };
 
 export function BrawlerProfile({brawler}: Readonly<BrawlerProfileProps>) {
   return (
-    <div className="flex gap-2 p-6 rounded-lg shadow-lg border bg-zinc-100 bg-opacity-90 m-2 max-w-screen-sm">
-      <BrawlerProfileImage brawler={brawler} size="2xl"/>
+    <div className="flex gap-2 p-6 rounded-lg shadow-lg border bg-zinc-100/90 m-2 max-w-lg">
+      <BrawlerProfileImage brawler={brawler} size="xl"/>
       <div className="flex flex-col gap-1">
         <div className="flex gap-1 items-center">
-          <div className="font-bold text-4xl">{brawler.name}</div>
+          <div className="font-bold sm:text-2xl text-xl">{brawler.name}</div>
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger>
@@ -31,7 +31,7 @@ export function BrawlerProfile({brawler}: Readonly<BrawlerProfileProps>) {
           </TooltipProvider>
         </div>
         <div>
-          <span className={cn("text-zinc-50", "text-lg", "font-bold", "rounded-md", "border", "border-zinc-800", "border-[0.5px]", "px-2", "py-1", "items-center", brawlerBackgroundColor(brawler))}>
+          <span className={cn("text-zinc-50", "sm:text-base text-sm", "font-bold", "rounded-md", "border", "border-zinc-800", "border-[0.5px]", "px-2", "py-1", "items-center", brawlerBackgroundColor(brawler))}>
             <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
               {brawlerRarityTitle(brawler.rarity)}
             </span>
