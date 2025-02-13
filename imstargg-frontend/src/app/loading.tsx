7022e6f9aee669f6a873abc0c7f69b4060140243
Youngrae Cho <yrc9229@gmail.com>
2025-02-13 10:00:17 +0900

    feat(front): 통계 컴포넌트 lazy loading

--- imstargg-frontend/src/app/loading.tsx
+++ imstargg-frontend/src/app/loading.tsx
@@ -1,9 +1,9 @@
-import { LoaderIcon } from "lucide-react";
+import { LoaderCircleIcon } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <LoaderIcon className="animate-spin w-8 h-8 sm:w-12 sm:h-12" />
+    <div className="flex items-center justify-center p-4">
+      <LoaderCircleIcon className="animate-spin w-8 h-8 sm:w-12 sm:h-12" />
     </div>
   );
 } 
