a74d4add526f4f665542671117f51836e88cb9c3
Youngrae Cho <yrc9229@gmail.com>
2025-02-17 14:40:26 +0900

    fix(brawlstars-client): 에러 디코더 설정 수정

--- client/brawlstars-client/src/main/java/com/imstargg/client/brawlstars/BrawlStarsClientConfig.java
+++ client/brawlstars-client/src/main/java/com/imstargg/client/brawlstars/BrawlStarsClientConfig.java
@@ -1,6 +1,5 @@
 package com.imstargg.client.brawlstars;
 
-import com.imstargg.client.core.DefaultClientErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +11,5 @@ class BrawlStarsClientConfig {
             BrawlStarsApi brawlStarsApi
     ) {
         return new BrawlStarsClient(brawlStarsApi);
-    }
-
-    @Bean
-    public BrawlStarsClientErrorDecoder brawlStarsClientErrorDecoder(DefaultClientErrorDecoder defaultClientErrorDecoder) {
-        return new BrawlStarsClientErrorDecoder(defaultClientErrorDecoder);
     }
 }
