ad29b5aeaf83e2e55f866635fe6ed66e3d48b402
Youngrae Cho <yrc9229@gmail.com>
2025-02-17 15:59:33 +0900

    chore: 이벤트 로테이션 잡 k8s

--- kubernetes/install-batch.sh
+++ kubernetes/install-batch.sh
@@ -2,5 +2,6 @@ sudo microk8s helm install -n app battle-event-update-job-batch ./helm/batch -f 
 sudo microk8s helm install -n app brawler-count-job-batch ./helm/batch -f brawler-count-job-values.yaml
 sudo microk8s helm install -n app brawler-ranking-job-batch ./helm/batch -f brawler-ranking-job-values.yaml
 sudo microk8s helm install -n app daily-job-batch ./helm/batch -f daily-job-values.yaml
+sudo microk8s helm install -n app event-rotation-job-batch ./helm/batch -f event-rotation-job-values.yaml
 sudo microk8s helm install -n app player-ranking-job-batch ./helm/batch -f player-ranking-job-values.yaml
 sudo microk8s helm install -n app player-update-job-batch ./helm/batch -f player-update-job-values.yaml
