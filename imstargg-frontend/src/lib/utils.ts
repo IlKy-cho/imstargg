e47413eb0dc666f586bb52d978eb4542c1aca3eb
Youngrae Cho <yrc9229@gmail.com>
2025-02-17 16:21:32 +0900

    feat(front): 배틀 이벤트 컴포넌트 수정

--- imstargg-frontend/src/lib/utils.ts
+++ imstargg-frontend/src/lib/utils.ts
@@ -8,5 +8,5 @@ export function cn(...inputs: ClassValue[]) {
 // 내가 추가한 것
 
 export function cnWithDefault(...inputs: ClassValue[]) {
-  return twMerge(clsx("p-2 border border-zinc-200 rounded", inputs));
+  return twMerge(clsx("sm:p-2 p-1 border border-zinc-200 rounded", inputs));
 }
