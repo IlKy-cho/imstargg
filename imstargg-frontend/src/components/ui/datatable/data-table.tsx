8075f43bfe20e79e02c8cf3e4b5e4d95804ee21a
Youngrae Cho <yrc9229@gmail.com>
2025-02-18 11:48:56 +0900

    feat(front): 통계 없을 경우 코멘트 수정

--- imstargg-frontend/src/components/ui/datatable/data-table.tsx
+++ imstargg-frontend/src/components/ui/datatable/data-table.tsx
@@ -86,7 +86,7 @@ export function DataTable<TData, TValue>(
             ) : (
               <TableRow>
                 <TableCell colSpan={columns.length} className="h-24 text-center">
-                  No results.
+                  통계가 존재하지 않거나 준비중입니다.
                 </TableCell>
               </TableRow>
             )}
