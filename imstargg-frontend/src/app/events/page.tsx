e65e79a604d39aba6d51fa33cecbfa98cfac9617
Youngrae Cho <yrc9229@gmail.com>
2025-02-20 22:15:54 +0900

    fix(front): 이벤트 목록 캐싱 수정

--- imstargg-frontend/src/app/events/page.tsx
+++ imstargg-frontend/src/app/events/page.tsx
@@ -1,12 +1,11 @@
-import {getBattleEvents, getRotationBattleEvents, getSoloRankBattleEvents} from "@/lib/api/battle-event";
 import {Metadata} from "next";
-import {BattleEventList, GroupedBattleEventList} from "@/components/battle-event";
 import {PageHeader, pageHeaderContainerDefault} from "@/components/page-header";
-import {cn, cnWithDefault} from "@/lib/utils";
-import {Separator} from "@/components/ui/separator";
-import {BrawlStarsIconSrc} from "@/lib/icon";
-import Image from "next/image";
-import {SwordsIcon} from "lucide-react";
+import {cn} from "@/lib/utils";
+import {
+  PageGroupedBattleEventList,
+  PageRotationBattleEventList,
+  PageSoloRankBattleEventList
+} from "./_components/event-list";
 
 export const metadata: Metadata = {
   title: `브롤스타즈 이벤트`,
@@ -33,57 +32,3 @@ export default async function EventsPage() {
   );
 }
 
-async function PageRotationBattleEventList() {
-
-  const rotationBattleEvents = await getRotationBattleEvents();
-  return (
-    <div className={cnWithDefault("p-1 flex flex-col gap-2")}>
-      <div className="flex items-center gap-1">
-        <Image
-          src={BrawlStarsIconSrc.TROPHY}
-          width={32}
-          height={32}
-          alt="trophy-icon"
-          className="sm:h-6 h-5 w-fit"
-        />
-        <h2 className="text-lg sm:text-xl font-bold">진행중</h2>
-      </div>
-      <Separator/>
-      <BattleEventList battleEvents={rotationBattleEvents.map(event => event.event)}/>
-    </div>
-  )
-}
-
-async function PageSoloRankBattleEventList() {
-  const battleEvents = await getSoloRankBattleEvents();
-  return (
-    <div className={cnWithDefault("p-1 flex flex-col gap-2")}>
-      <div className="flex items-center gap-1">
-        <Image
-          src={BrawlStarsIconSrc.RANKED_FRONT}
-          width={32}
-          height={32}
-          alt="trophy-icon"
-          className="sm:h-6 h-5 w-fit"
-        />
-        <h2 className="text-lg sm:text-xl font-bold">경쟁전</h2>
-      </div>
-      <Separator/>
-      <BattleEventList battleEvents={battleEvents}/>
-    </div>
-  )
-}
-
-async function PageGroupedBattleEventList() {
-  const battleEvents = await getBattleEvents();
-  return (
-    <div className={cnWithDefault("p-1 flex flex-col gap-2")}>
-      <div className="flex items-center gap-1">
-        <SwordsIcon className="sm:h-6 h-5 w-fit"/>
-        <h2 className="text-lg sm:text-xl font-bold">최근 1주일</h2>
-      </div>
-      <Separator/>
-      <GroupedBattleEventList battleEvents={battleEvents}/>
-    </div>
-  )
-}
