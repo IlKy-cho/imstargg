42707062c88498283f60d5d352df2de8687662bb
Youngrae Cho <yrc9229@gmail.com>
2025-02-20 10:57:25 +0900

    chore: 통계 배치 배포

--- kubernetes/install-batch.sh
+++ kubernetes/install-batch.sh
@@ -1,7 +1,11 @@
 sudo microk8s helm install -n app battle-event-update-job-batch ./helm/batch -f battle-event-update-job-values.yaml
 sudo microk8s helm install -n app brawler-count-job-batch ./helm/batch -f brawler-count-job-values.yaml
 sudo microk8s helm install -n app brawler-ranking-job-batch ./helm/batch -f brawler-ranking-job-values.yaml
-sudo microk8s helm install -n app statistics-job-batch ./helm/batch -f statistics-job-values.yaml
 sudo microk8s helm install -n app event-rotation-job-batch ./helm/batch -f event-rotation-job-values.yaml
 sudo microk8s helm install -n app player-ranking-job-batch ./helm/batch -f player-ranking-job-values.yaml
 sudo microk8s helm install -n app player-update-job-batch ./helm/batch -f player-update-job-values.yaml
+
+sudo microk8s helm install -n app today-statistics-job-batch ./helm/batch -f today-statistics-job-values.yaml
+sudo microk8s helm install -n app one-day-statistics-job-batch ./helm/batch -f one-day-statistics-job-values.yaml
+sudo microk8s helm install -n app two-days-statistics-job-batch ./helm/batch -f two-days-statistics-job-values.yaml
+sudo microk8s helm install -n app three-days-statistics-job-batch ./helm/batch -f three-days-statistics-job-values.yaml
