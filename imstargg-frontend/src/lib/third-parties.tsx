4800d5bc8fca58286989f7e57f1f7368ce93675b
Youngrae Cho <yrc9229@gmail.com>
2025-03-06 16:17:21 +0900

    feat(front): 구글 애드센스 스크립트 추가

--- imstargg-frontend/src/lib/third-parties.tsx
+++ imstargg-frontend/src/lib/third-parties.tsx
@@ -1,4 +1,5 @@
 import {GoogleAnalytics as NextGoogleAnalytics} from '@next/third-parties/google'
+import Script from "next/script";
 
 
 export function GoogleAnalytics() {
@@ -6,5 +7,16 @@ export function GoogleAnalytics() {
     process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
       <NextGoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}/>
     )
+  );
+}
+
+export function GoogleAdSense() {
+  return (
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1486181177977193"
+      crossOrigin="anonymous"
+      strategy="lazyOnload"
+    />
   );
 }
