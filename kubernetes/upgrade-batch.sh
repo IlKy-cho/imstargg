ea60a7b561503f3c617c96c93ff01430dbfc16da
Youngrae Cho <yrc9229@gmail.com>
2025-03-10 21:38:39 +0900

    chore: 갱신 실패 잡 배포

--- kubernetes/upgrade-batch.sh
+++ kubernetes/upgrade-batch.sh
@@ -3,6 +3,7 @@ sudo microk8s helm upgrade -n app brawler-count-job-batch ./helm/batch -f brawle
 sudo microk8s helm upgrade -n app brawler-ranking-job-batch ./helm/batch -f brawler-ranking-job-values.yaml
 sudo microk8s helm upgrade -n app event-rotation-job-batch ./helm/batch -f event-rotation-job-values.yaml
 sudo microk8s helm upgrade -n app player-ranking-job-batch ./helm/batch -f player-ranking-job-values.yaml
+sudo microk8s helm upgrade -n app player-renewal-failed-job-batch ./helm/batch -f player-renewal-failed-job-values.yaml
 sudo microk8s helm upgrade -n app player-update-job-batch ./helm/batch -f player-update-job-values.yaml
 
 sudo microk8s helm upgrade -n app today-statistics-job-batch ./helm/batch -f today-statistics-job-values.yaml
