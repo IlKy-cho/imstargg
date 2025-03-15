224fe52aa3d118d33fd830b98f59b139bdcef300
Youngrae Cho <yrc9229@gmail.com>
2025-03-15 12:10:11 +0900

    test: 멀티 스레드 메서드 추가

--- tests/java-test-util/src/main/java/com/imstargg/test/java/MultiThreadUtil.java
+++ tests/java-test-util/src/main/java/com/imstargg/test/java/MultiThreadUtil.java
@@ -7,10 +7,19 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.function.IntConsumer;
 import java.util.function.IntFunction;
 import java.util.stream.IntStream;
 
 public abstract class MultiThreadUtil {
+
+    public static void execute(Runnable runnable, int threadCount) throws Exception {
+        executeTasks(i -> () -> { runnable.run(); return null;}, threadCount);
+    }
+
+    public static void execute(IntConsumer consumer, int threadCount) throws Exception {
+        executeTasks(i -> () -> { consumer.accept(i); return null;}, threadCount);
+    }
 
     public static <T> List<T> execute(Callable<T> callable, int threadCount) throws Exception {
         return executeTasks(i -> callable, threadCount);
