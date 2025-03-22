4bc2926c77e80c498b8ec767a7e5205b94763fb3
Youngrae Cho <yrc9229@gmail.com>
2025-03-22 21:49:02 +0900

    feat(core): 경쟁전 이벤트 조회 수정

--- core/core-domain/src/main/java/com/imstargg/core/domain/brawlstars/BattleEventCache.java
+++ core/core-domain/src/main/java/com/imstargg/core/domain/brawlstars/BattleEventCache.java
@@ -1,12 +1,18 @@
 package com.imstargg.core.domain.brawlstars;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.imstargg.core.domain.BrawlStarsId;
+import com.imstargg.core.support.ObjectMapperHelper;
+import com.imstargg.storage.db.core.cache.CacheKeyBuilder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @Component
 public class BattleEventCache {
@@ -15,6 +21,13 @@ public class BattleEventCache {
             .maximumSize(50)
             .expireAfterWrite(Duration.ofHours(1))
             .build();
+    private final ObjectMapperHelper objectMapperHelper;
+    private final StringRedisTemplate redisTemplate;
+
+    public BattleEventCache(ObjectMapperHelper objectMapperHelper, StringRedisTemplate redisTemplate) {
+        this.objectMapperHelper = objectMapperHelper;
+        this.redisTemplate = redisTemplate;
+    }
 
     public void set(BrawlStarsId id, BattleEvent battleEvent) {
         caffeine.put(id, battleEvent);
@@ -22,5 +35,25 @@ public class BattleEventCache {
 
     public Optional<BattleEvent> find(BrawlStarsId id) {
         return Optional.ofNullable(caffeine.getIfPresent(id));
+    }
+
+    public List<BrawlStarsId> getSoloRankEventIds(Supplier<List<BrawlStarsId>> mappingFunction) {
+        String key = new CacheKeyBuilder("event", "v1")
+                .add("solo-rank-event-ids")
+                .build();
+        return Optional.ofNullable(
+                        redisTemplate.opsForValue().get(key)
+                ).map(value -> objectMapperHelper.read(value, new TypeReference<List<Long>>() {
+                }))
+                .orElseGet(() -> {
+                    List<Long> soloRankEventIds = mappingFunction.get().stream()
+                            .map(BrawlStarsId::value)
+                            .toList();
+                    redisTemplate.opsForValue()
+                            .set(key, objectMapperHelper.write(soloRankEventIds), Duration.ofHours(1));
+                    return soloRankEventIds;
+                }).stream()
+                .map(BrawlStarsId::new)
+                .toList();
     }
 }
