030ad7fd0764f31c2751e4c80f3e61f824bb5af4
Youngrae Cho <yrc9229@gmail.com>
2025-03-30 12:11:44 +0900

    feat(db-collection): 통계 수집 배틀 엔티티 추가

---
+++ storage/db-collection/src/main/java/com/imstargg/storage/db/core/statistics/StatisticsCollectedBattleCollectionJpaRepository.java
@@ -0,0 +1,8 @@
+package com.imstargg.storage.db.core.statistics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatisticsCollectedBattleCollectionJpaRepository
+        extends JpaRepository<StatisticsCollectedBattleCollectionEntity, Long> {
+
+}
