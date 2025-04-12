33e62e33164ad95a277eca47b8e1f78479f67554
Youngrae Cho <yrc9229@gmail.com>
2025-04-12 19:03:56 +0900

    feat(db-collection): 통계 체크포인트 날짜 추가

--- storage/db-collection/src/main/java/com/imstargg/storage/db/core/statistics/StatisticsCheckPointCollectionJpaRepository.java
+++ storage/db-collection/src/main/java/com/imstargg/storage/db/core/statistics/StatisticsCheckPointCollectionJpaRepository.java
@@ -2,10 +2,11 @@ package com.imstargg.storage.db.core.statistics;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface StatisticsCheckPointCollectionJpaRepository
         extends JpaRepository<StatisticsCheckPointCollectionEntity, Long> {
 
-    Optional<StatisticsCheckPointCollectionEntity> findByKey(String key);
+    Optional<StatisticsCheckPointCollectionEntity> findByKeyAndBattleDate(String key, LocalDate battleDate);
 }
