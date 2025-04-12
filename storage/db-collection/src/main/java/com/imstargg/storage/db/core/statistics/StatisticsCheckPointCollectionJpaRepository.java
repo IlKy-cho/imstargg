7bbe90ffb814934dbee400222a81e2ab2477164e
Youngrae Cho <yrc9229@gmail.com>
2025-04-12 15:17:08 +0900

    feat(batch): 통계 체크포인터

--- storage/db-collection/src/main/java/com/imstargg/storage/db/core/statistics/StatisticsCheckPointCollectionJpaRepository.java
+++ storage/db-collection/src/main/java/com/imstargg/storage/db/core/statistics/StatisticsCheckPointCollectionJpaRepository.java
@@ -2,7 +2,10 @@ package com.imstargg.storage.db.core.statistics;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StatisticsCheckPointCollectionJpaRepository
         extends JpaRepository<StatisticsCheckPointCollectionEntity, Long> {
 
+    Optional<StatisticsCheckPointCollectionEntity> findByKey(String key);
 }
