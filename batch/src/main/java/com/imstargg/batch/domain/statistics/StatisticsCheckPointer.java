26542825f4af7438b47305ff637880ddb0988f21
Youngrae Cho <yrc9229@gmail.com>
2025-04-13 09:21:33 +0900

    feat(batch): 체크포인터

--- batch/src/main/java/com/imstargg/batch/domain/statistics/StatisticsCheckPointer.java
+++ batch/src/main/java/com/imstargg/batch/domain/statistics/StatisticsCheckPointer.java
@@ -2,42 +2,28 @@ package com.imstargg.batch.domain.statistics;
 
 import com.imstargg.storage.db.core.statistics.StatisticsCheckPointCollectionEntity;
 import com.imstargg.storage.db.core.statistics.StatisticsCheckPointCollectionJpaRepository;
-import jakarta.annotation.Nullable;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
+@Component
 public class StatisticsCheckPointer {
-
-    private final String key;
 
     private final StatisticsCheckPointCollectionJpaRepository repository;
 
-    @Nullable
-    private StatisticsCheckPointCollectionEntity entity;
-
     public StatisticsCheckPointer(
-            String key,
             StatisticsCheckPointCollectionJpaRepository statisticsCheckPointCollectionJpaRepository
     ) {
-        this.key = key;
         this.repository = statisticsCheckPointCollectionJpaRepository;
     }
 
-    public long getLastBattleId() {
-        return getEntity().getLastBattleId();
+    public StatisticsCheckPointCollectionEntity get(String key, LocalDate battleDate) {
+        return repository.findByKeyAndBattleDate(key, battleDate)
+                .orElseGet(() -> repository.save(new StatisticsCheckPointCollectionEntity(key, battleDate)));
     }
 
-    public void update(long battleId) {
-        getEntity().updateLastBattleId(battleId);
-    }
 
-    public void save() {
-        repository.save(getEntity());
-    }
-
-    private StatisticsCheckPointCollectionEntity getEntity() {
-        if (entity == null) {
-            entity = repository.findByKey(key)
-                    .orElseGet(() -> repository.save(new StatisticsCheckPointCollectionEntity(key)));
-        }
-        return entity;
+    public void save(StatisticsCheckPointCollectionEntity entity) {
+        repository.save(entity);
     }
 }
