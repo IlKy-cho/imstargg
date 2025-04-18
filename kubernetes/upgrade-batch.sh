88ac6ec2d3c8159f118bbd7e7629ef8a2bf5c4f1
Youngrae Cho <yrc9229@gmail.com>
2025-04-18 10:14:38 +0900

    fix(chore): 배치 배포 스크립트 수정

--- kubernetes/upgrade-batch.sh
+++ kubernetes/upgrade-batch.sh
@@ -5,9 +5,9 @@ sudo microk8s helm upgrade -n app player-ranking-job-batch ./helm/batch -f playe
 sudo microk8s helm upgrade -n app player-renewal-failed-job-batch ./helm/batch -f player-renewal-failed-job-values.yaml
 sudo microk8s helm upgrade -n app player-update-job-batch ./helm/batch -f player-update-job-values.yaml
 
-sudo microk8s helm upgrade -n app player-brawler-statstics-job-batch ./helm/batch -f playerBrawlerStatstics-values.yaml
-sudo microk8s helm upgrade -n app brawlerBattleRankStatisticsJob-batch ./helm/batch -f brawlerBattleRankStatisticsJob-values.yaml
-sudo microk8s helm upgrade -n app brawlerBattleResultStatisticsJob-batch ./helm/batch -f brawlerBattleResultStatisticsJob-values.yaml
-sudo microk8s helm upgrade -n app brawlerEnemyBattleResultStatisticsJob-batch ./helm/batch -f brawlerEnemyBattleResultStatisticsJob-values.yaml
-sudo microk8s helm upgrade -n app brawlerPairBattleRankStatisticsJob-batch ./helm/batch -f brawlerPairBattleRankStatisticsJob-values.yaml
-sudo microk8s helm upgrade -n app brawlerPairBattleResultStatisticsJob-batch ./helm/batch -f brawlerPairBattleResultStatisticsJob-values.yaml
+sudo microk8s helm upgrade -n app player-brawler-statstics-job-batch ./helm/batch -f playerBrawlerStatisticsJob-values.yaml
+sudo microk8s helm upgrade -n app brawler-battle-rank-statistics-job-batch ./helm/batch -f brawlerBattleRankStatisticsJob-values.yaml
+sudo microk8s helm upgrade -n app brawler-battle-result-statistics-job-batch ./helm/batch -f brawlerBattleResultStatisticsJob-values.yaml
+sudo microk8s helm upgrade -n app brawler-enemy-battle-result-statistics-job-batch ./helm/batch -f brawlerEnemyBattleResultStatisticsJob-values.yaml
+sudo microk8s helm upgrade -n app brawler-pair-battle-rank-statistics-job-batch ./helm/batch -f brawlerPairBattleRankStatisticsJob-values.yaml
+sudo microk8s helm upgrade -n app brawler-pair-battle-result-statistics-job-batch ./helm/batch -f brawlerPairBattleResultStatisticsJob-values.yaml
