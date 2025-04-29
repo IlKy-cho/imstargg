69cb5fe8644ba775bc9c9285db90eda6a094c956
Youngrae Cho <yrc9229@gmail.com>
2025-04-29 21:24:36 +0900

    feat(worker): 리스너 커밋 설정

--- worker/src/main/java/com/imstargg/worker/handler/PlayerRenewalEventHandler.java
+++ worker/src/main/java/com/imstargg/worker/handler/PlayerRenewalEventHandler.java
@@ -5,6 +5,7 @@ import com.imstargg.support.alert.AlertCommand;
 import com.imstargg.support.alert.AlertManager;
 import com.imstargg.worker.domain.PlayerRenewalService;
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import io.awspring.cloud.sqs.listener.acknowledgement.Acknowledgement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -24,13 +25,14 @@ class PlayerRenewalEventHandler {
 
     @SqsListener(
             queueNames = "${app.event.queue.player-renewal.name}",
-            pollTimeoutSeconds = "10"
+            acknowledgementMode = "MANUAL",
+            messageVisibilitySeconds = "10"
     )
-    void handlePlayerRenewalEvent(PlayerRenewalEvent event) {
-        log.debug("플레이어 갱신 이벤트 수신 event={}", event);
+    void handlePlayerRenewalEvent(PlayerRenewalEvent event, Acknowledgement acknowledgement) {
+        log.info("플레이어 갱신 이벤트 시작 tag={}", event.tag());
         try {
             playerRenewalService.renew(event.tag());
-            log.debug("플레이어 갱신 완료 tag={}", event.tag());
+            log.info("플레이어 갱신 완료 tag={}", event.tag());
         } catch (Exception ex) {
             log.error("플레이어 갱신 중 예외 발생. tag={}", event.tag(), ex);
             alertManager.alert(AlertCommand.builder()
@@ -40,6 +42,7 @@ class PlayerRenewalEventHandler {
                     .ex(ex)
                     .build());
         }
+        acknowledgement.acknowledgeAsync();
     }
 
 }
