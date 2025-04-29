0d263362576790312233bcbcc0340eb566874815
Youngrae Cho <yrc9229@gmail.com>
2025-04-29 21:23:42 +0900

    feat(worker): 리스너 팩토리 설정 수정

---
+++ worker/src/main/java/com/imstargg/worker/handler/SqsListenerConfig.java
@@ -0,0 +1,43 @@
+package com.imstargg.worker.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.awspring.cloud.autoconfigure.sqs.SqsProperties;
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import io.awspring.cloud.sqs.listener.ContainerOptionsBuilder;
+import io.awspring.cloud.sqs.support.converter.SqsMessagingMessageConverter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+@Configuration
+@EnableConfigurationProperties(SqsProperties.class)
+class SqsListenerConfig {
+
+    @Bean
+    SqsMessageListenerContainerFactory<Object> sqsListenerContainerFactory(
+            SqsAsyncClient sqsAsyncClient,
+            ObjectMapper objectMapper,
+            SqsProperties sqsProperties
+    ) {
+        return SqsMessageListenerContainerFactory.builder()
+                .sqsAsyncClient(sqsAsyncClient)
+                .configure(options -> configureContainerOptions(options, sqsProperties))
+                .configure(options -> configureObjectMapper(options, objectMapper))
+                .build();
+    }
+
+    private void configureContainerOptions(ContainerOptionsBuilder<?, ?> options, SqsProperties sqsProperties) {
+        PropertyMapper mapper = PropertyMapper.get().alwaysApplyingWhenNonNull();
+        mapper.from(sqsProperties.getListener().getMaxConcurrentMessages()).to(options::maxConcurrentMessages);
+        mapper.from(sqsProperties.getListener().getMaxMessagesPerPoll()).to(options::maxMessagesPerPoll);
+        mapper.from(sqsProperties.getListener().getPollTimeout()).to(options::pollTimeout);
+    }
+
+    private void configureObjectMapper(ContainerOptionsBuilder<?, ?> options, ObjectMapper objectMapper) {
+        var messageConverter = new SqsMessagingMessageConverter();
+        messageConverter.setObjectMapper(objectMapper);
+        options.messageConverter(messageConverter);
+    }
+}
