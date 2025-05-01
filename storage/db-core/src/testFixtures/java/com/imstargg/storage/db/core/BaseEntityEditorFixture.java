97fc4cb4cf795ea6aadc23e30e2ba25fce70e19b
Youngrae Cho <yrc9229@gmail.com>
2025-05-01 22:30:46 +0900

    test(db): PlayerCollectionEntityFixture 베이스 필드 수정 추가

--- storage/db-core/src/testFixtures/java/com/imstargg/storage/db/core/BaseEntityEditorFixture.java
+++ storage/db-core/src/testFixtures/java/com/imstargg/storage/db/core/BaseEntityEditorFixture.java
@@ -6,10 +6,12 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Stream;
 
 public class BaseEntityEditorFixture {
 
@@ -19,33 +21,9 @@ public class BaseEntityEditorFixture {
         this.entity = entity;
     }
 
-    public void editId(Object id) {
-        Class<?> entityClass = entity.getClass();
-        
-        Optional<Field> idField = Arrays.stream(entityClass.getDeclaredFields())
-                .filter(field -> field.isAnnotationPresent(Id.class))
-                .findFirst();
-
-        if (idField.isEmpty()) {
-            throw new IllegalArgumentException(entityClass.getSimpleName() +
-                    " 클래스에 @Id 어노테이션이 붙은 필드를 찾을 수 없습니다.");
-        }
-
-        Field field = idField.get();
-        field.setAccessible(true);
-
-        try {
-            field.set(entity, id);
-        } catch (IllegalAccessException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-
     public void editDeleted(boolean deleted) {
-        Class<?> entityClass = entity.getClass();
-
-        if (!BaseEntity.class.isAssignableFrom(entityClass)) {
-            throw new IllegalArgumentException(entityClass.getSimpleName() + " 클래스는 BaseEntity를 상속해야 합니다.");
+        if (!BaseEntity.class.isAssignableFrom(entity.getClass())) {
+            throw new IllegalArgumentException(entity.getClass().getName() + " 클래스는 BaseEntity를 상속해야 합니다.");
         }
 
         if (deleted) {
@@ -55,51 +33,73 @@ public class BaseEntityEditorFixture {
         }
     }
 
-    public void editCreatedAt(Object createdAt) {
-        Class<?> entityClass = entity.getClass();
+    public void editId(Object id) {
+        List<Field> idFields = findFieldByAnnotation(Id.class);
 
-        List<Field> createdAtFields = Arrays.stream(entityClass.getDeclaredFields())
-                .filter(field ->
-                        field.isAnnotationPresent(CreatedDate.class)
-                                || field.isAnnotationPresent(CreationTimestamp.class)
-                ).toList();
+        if (idFields.isEmpty()) {
+            throw new IllegalArgumentException(entity.getClass().getName() +
+                    " 클래스에 @Id 어노테이션이 붙은 필드를 찾을 수 없습니다.");
+        }
+
+        for (Field field : idFields) {
+            setFieldValue(field, id);
+        }
+    }
+
+    public void editCreatedAt(Object createdAt) {
+        List<Field> createdAtFields = Stream.of(
+                        findFieldByAnnotation(CreationTimestamp.class), findFieldByAnnotation(CreatedDate.class)
+                ).flatMap(List::stream).distinct().toList();
 
         if (createdAtFields.isEmpty()) {
-            throw new IllegalArgumentException(entityClass.getSimpleName() +
-                    " 클래스에 @CreatedDate 또는 @CreationTimestamp 어노테이션이 붙은 필드를 찾을 수 없습니다.");
+            throw new IllegalArgumentException(entity.getClass().getName() +
+                    " 클래스에 @CreationTimestamp 또는 @CreatedDate 어노테이션이 붙은 필드를 찾을 수 없습니다.");
         }
 
         for (Field field : createdAtFields) {
-            field.setAccessible(true);
-            try {
-                field.set(entity, createdAt);
-            } catch (IllegalAccessException e) {
-                throw new IllegalArgumentException(e);
-            }
+            setFieldValue(field, createdAt);
         }
     }
 
     public void editUpdatedAt(Object updatedAt) {
-        Class<?> entityClass = entity.getClass();
-
-        List<Field> updatedAtFields = Arrays.stream(entityClass.getDeclaredFields())
-                .filter(field ->
-                        field.isAnnotationPresent(LastModifiedDate.class)
-                                || field.isAnnotationPresent(UpdateTimestamp.class)
-                ).toList();
+        List<Field> updatedAtFields = Stream.of(
+                        findFieldByAnnotation(UpdateTimestamp.class), findFieldByAnnotation(LastModifiedDate.class)
+        ).flatMap(List::stream).distinct().toList();
 
         if (updatedAtFields.isEmpty()) {
-            throw new IllegalArgumentException(entityClass.getSimpleName() +
-                    " 클래스에 @LastModifiedDate 또는 @UpdateTimestamp 어노테이션이 붙은 필드를 찾을 수 없습니다.");
+            throw new IllegalArgumentException(entity.getClass().getName() +
+                    " 클래스에 @UpdateTimestamp 또는 @LastModifiedDate 어노테이션이 붙은 필드를 찾을 수 없습니다.");
         }
 
         for (Field field : updatedAtFields) {
-            field.setAccessible(true);
-            try {
-                field.set(entity, updatedAt);
-            } catch (IllegalAccessException e) {
-                throw new IllegalArgumentException(e);
-            }
+            setFieldValue(field, updatedAt);
+        }
+    }
+
+    private <T extends Annotation> List<Field> findFieldByAnnotation(Class<T> annotationClass) {
+        List<Field> foundFields = new ArrayList<>();
+        Class<?> currentClass = entity.getClass();
+
+        while (currentClass != null && currentClass != Object.class) {
+            List<Field> fields = Arrays.stream(currentClass.getDeclaredFields())
+                    .filter(f -> f.isAnnotationPresent(annotationClass))
+                    .toList();
+
+            foundFields.addAll(fields);
+
+            currentClass = currentClass.getSuperclass();
+        }
+
+        return foundFields.stream().toList();
+    }
+
+    private void setFieldValue(Field field, Object value) {
+        field.setAccessible(true); // private 필드에 접근 가능하도록 설정
+
+        try {
+            field.set(entity, value); // 필드 값 변경
+        } catch (IllegalAccessException e) {
+            throw new IllegalStateException("필드에 접근할 수 없습니다: " + e.getMessage(), e);
         }
     }
 }
